4a27691315ffe2f535f1c000cce5048e5f308f9e
macrogreg <macrogreg@live.com>
2020-08-20 12:03:29 -0700

    Clean up auxiliary Datadog files, leave all Tracer sources and tests in-tact.

--- createLogPath.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-mkdir -p /var/log/datadog/dotnet
-chmod a+rwx /var/log/datadog/dotnet
--- reproduction-dependencies/Datadog.StackExchange.Redis.Abstractions/ICache.cs
+++
@@ -1,9 +0,0 @@
-namespace Datadog.StackExchange.Redis.Abstractions
-{
-    public interface ICache
-    {
-        string GetString(string key);
-
-        void SetString(string key, string value);
-    }
-}
