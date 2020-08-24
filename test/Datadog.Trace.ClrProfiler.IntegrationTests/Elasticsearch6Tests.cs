e14566b8660cea3815c23a3c882d7c1463d8f766
Paulo Janotti <pjanotti@splunk.com>
2020-08-24 11:39:30 -0700

    Add files required by `docker-compose run build`

---
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/Elasticsearch6Tests.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Datadog.Core.Tools;
+using Datadog.Trace.TestHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests
+{
+    public class Elasticsearch6Tests : TestHelper
+    {
+        public Elasticsearch6Tests(ITestOutputHelper output)
+            : base("Elasticsearch", output)
+        {
+            SetServiceVersion("1.0.0");
+        }
+
+        [Theory]
+        [MemberData(nameof(PackageVersions.ElasticSearch6), MemberType = typeof(PackageVersions))]
+        [Trait("Category", "EndToEnd")]
+        public void SubmitsTraces(string packageVersion)
+        {
+            int agentPort = TcpPortProvider.GetOpenPort();
+
+            using (var agent = new MockTracerAgent(agentPort))
+            using (var processResult = RunSampleAndWaitForExit(agent.Port, packageVersion: packageVersion))
+            {
+                Assert.True(processResult.ExitCode >= 0, $"Process exited with code {processResult.ExitCode}");
+
+                var expected = new List<string>();
+
+                // commands with sync and async
+                for (var i = 0; i < 2; i++)
+                {
+                    expected.AddRange(new List<string>
+                    {
+                        "Bulk",
+                        "Create",
+                        "Search",
+                        "DeleteByQuery",
+
+                        "CreateIndex",
+                        "IndexExists",
+                        "UpdateIndexSettings",
+                        "BulkAlias",
+                        "GetAlias",
+                        "PutAlias",
+                        "AliasExists",
+                        "DeleteAlias",
+                        "DeleteAlias",
+                        "CreateIndex",
+                        "SplitIndex", // Only present on 6.1+
+                        "DeleteIndex",
+                        "CloseIndex",
+                        "OpenIndex",
+                        "PutIndexTemplate",
+                        "IndexTemplateExists",
+                        "DeleteIndexTemplate",
+                        "IndicesShardStores",
+                        "IndicesStats",
+                        "DeleteIndex",
+                        "GetAlias",
+
+                        "CatAliases",
+                        "CatAllocation",
+                        "CatCount",
+                        "CatFielddata",
+                        "CatHealth",
+                        "CatHelp",
+                        "CatIndices",
+                        "CatMaster",
+                        "CatNodeAttributes",
+                        "CatNodes",
+                        "CatPendingTasks",
+                        "CatPlugins",
+                        "CatRecovery",
+                        "CatRepositories",
+                        "CatSegments",
+                        "CatShards",
+                        // "CatSnapshots",
+                        "CatTasks",
+                        "CatTemplates",
+                        "CatThreadPool",
+
+                        "PutJob",
+                        "ValidateJob",
+                        "GetInfluencers",
+                        "GetJobs",
+                        "GetJobStats",
+                        "GetModelSnapshots",
+                        "FlushJob",
+                        "GetOverallBuckets", // Only present on 6.1+
+                        "ForecastJob", // Only present on 6.1+
+                        "GetAnomalyRecords",
+                        "GetBuckets",
+                        "GetCategories",
+                        "CloseJob",
+                        "OpenJob",
+                        "DeleteJob",
+
+                        "ClusterAllocationExplain",
+                        "ClusterGetSettings",
+                        "ClusterHealth",
+                        "ClusterPendingTasks",
+                        "ClusterPutSettings",
+                        "ClusterReroute",
+                        "ClusterState",
+                        "ClusterStats",
+
+                        "PutRole",
+                        "PutRoleMapping",
+                        "GetRole",
+                        "GetRoleMapping",
+                        "DeleteRoleMapping",
+                        "DeleteRole",
+                        "PutUser",
+                        "ChangePassword",
+                        "GetUser",
+                        "DisableUser",
+                        "DeleteUser",
+                    });
+
+                    if (string.IsNullOrEmpty(packageVersion) || string.Compare(packageVersion, "6.1.0", StringComparison.Ordinal) < 0)
+                    {
+                        expected.Remove("SplitIndex");
+                        expected.Remove("GetOverallBuckets");
+                        expected.Remove("ForecastJob");
+                    }
+                }
+
+                var spans = agent.WaitForSpans(expected.Count)
+                                 .Where(s => s.Type == "elasticsearch")
+                                 .OrderBy(s => s.Start)
+                                 .ToList();
+
+                foreach (var span in spans)
+                {
+                    Assert.Equal("elasticsearch.query", span.Name);
+                    Assert.Equal("Samples.Elasticsearch-elasticsearch", span.Service);
+                    Assert.Equal("elasticsearch", span.Type);
+                    Assert.False(span.Tags?.ContainsKey(Tags.Version), "External service span should not have service version tag.");
+                }
+
+                ValidateSpans(spans, (span) => span.Resource, expected);
+            }
+        }
+    }
+}
---
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/Helpers/GraphQLSpanExpectation.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Datadog.Trace.TestHelpers;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests
+{
+    public class GraphQLSpanExpectation : WebServerSpanExpectation
+    {
+        public GraphQLSpanExpectation(string serviceName, string operationName, string resourceName)
+            : base(serviceName, serviceVersion: null, operationName, resourceName, SpanTypes.GraphQL)
+        {
+            RegisterDelegateExpectation(ExpectErrorMatch);
+            RegisterTagExpectation(nameof(Tags.GraphQLSource), expected: GraphQLSource);
+            RegisterTagExpectation(nameof(Tags.GraphQLOperationType), expected: GraphQLOperationType);
+        }
+
+        public string GraphQLRequestBody { get; set; }
+
+        public string GraphQLOperationType { get; set; }
+
+        public string GraphQLOperationName { get; set; }
+
+        public string GraphQLSource { get; set; }
+
+        public bool IsGraphQLError { get; set; }
+
+        private IEnumerable<string> ExpectErrorMatch(MockTracerAgent.Span span)
+        {
+            var error = GetTag(span, Tags.ErrorMsg);
+            if (string.IsNullOrEmpty(error))
+            {
+                if (IsGraphQLError)
+                {
+                    yield return $"Expected an error message but {Tags.ErrorMsg} tag is missing or empty.";
+                }
+            }
+            else
+            {
+                if (!IsGraphQLError)
+                {
+                    yield return $"Expected no error message but {Tags.ErrorMsg} tag was {error}.";
+                }
+            }
+        }
+    }
+}
