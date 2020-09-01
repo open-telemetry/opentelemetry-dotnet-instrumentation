09bed2ab67178639023a9260b8620d22de6be802
Paulo Janotti <pjanotti@splunk.com>
2020-09-01 16:37:11 -0700

    Enable azure pipelines (#12)
    * Add files required by `docker-compose run build`
    
    * Enabled local `docker-compose run IntegrationTests`
    
    * Added files for Windows integration tests
    
    * Add Azure Pipeline files
    
    * Add Datadog.Trace.MSBuild
    
    * Datadog.Trace.MSBuild using Newtonsoft.Json
    
    * Add Datadog.Trace.MSBuild to sln to include it in nuget restore
    
    * Add EntityFramework6x.MdTokenLookupFailure to sln so nuget restore works
    
    * Add reproductions folder to sln
    
    * Add apline docker files
    
    * Add Datadog.StackExchange.Redis.StrongName to sln
    
    * Add samples-iis (integration tests for Windows)
    
    * Add Datadog.StackExchange.Redis to sln
    
    * Add Datadog.StackExchange.Redis.Abstractions to sln
    
    * Add ./docker/iis/LoaderOptimizationRegKey.dockerfile
    
    * Run only netcoreapp3.1 for Linux for now
    
    * Add deploy wixproj
    
    * Add log projects used in IntegrationTests on Windows

---
+++ reproductions/HttpMessageHandler.StackOverflowException/Program.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Datadog.Trace;
+using Datadog.Trace.ClrProfiler;
+
+namespace HttpMessageHandler.StackOverflowException
+{
+    internal class Program
+    {
+        private static async Task<int> Main()
+        {
+            try
+            {
+                Console.WriteLine($"Profiler attached: {Instrumentation.ProfilerAttached}");
+
+                var baseAddress = new Uri("https://www.example.com/");
+                var regularHttpClient = new HttpClient { BaseAddress = baseAddress };
+                var customHandlerHttpClient = new HttpClient(new DerivedHandler()) { BaseAddress = baseAddress };
+
+                using (var scope = Tracer.Instance.StartActive("main"))
+                {
+                    Console.WriteLine("Calling regularHttpClient.GetAsync");
+                    await regularHttpClient.GetAsync("default-handler");
+                    Console.WriteLine("Called regularHttpClient.GetAsync");
+
+                    Console.WriteLine("Calling customHandlerHttpClient.GetAsync");
+                    await customHandlerHttpClient.GetAsync("derived-handler");
+                    Console.WriteLine("Called customHandlerHttpClient.GetAsync");
+                }
+
+                Console.WriteLine("No stack overflow exceptions!");
+                Console.WriteLine("All is well!");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                return (int)ExitCode.UnknownError;
+            }
+
+            return (int)ExitCode.Success;
+        }
+    }
+
+    public class DerivedHandler : HttpClientHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            Tracer.Instance.ActiveScope?.Span.SetTag("class", nameof(DerivedHandler));
+
+            Console.WriteLine("Calling base.SendAsync()");
+            var result = await base.SendAsync(request, cancellationToken);
+            Console.WriteLine("Called base.SendAsync()");
+            return result;
+        }
+    }
+
+    enum ExitCode : int
+    {
+        Success = 0,
+        UnknownError = -10
+    }
+}
---
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetWebFormsTests.cs
@@ -0,0 +1,88 @@
+#if NET461 || NET452
+
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests
+{
+    public class AspNetWebFormsTests : TestHelper, IClassFixture<IisFixture>
+    {
+        private readonly IisFixture _iisFixture;
+
+        // NOTE: Would pass this in addition to the name/output to the new constructor if we removed the Samples.WebForms copied project in favor of the demo repo source project...
+        // $"../dd-trace-demo/dotnet-coffeehouse/Datadog.Coffeehouse.WebForms",
+        public AspNetWebFormsTests(IisFixture iisFixture, ITestOutputHelper output)
+            : base("WebForms", "samples-aspnet", output)
+        {
+            SetServiceVersion("1.0.0");
+
+            _iisFixture = iisFixture;
+            _iisFixture.TryStartIis(this);
+        }
+
+        [Theory]
+        [Trait("Category", "EndToEnd")]
+        [Trait("Integration", nameof(AspNetWebFormsTests))]
+        [InlineData("/Account/Login", "GET /account/login", false)]
+        public async Task SubmitsTraces(
+            string path,
+            string expectedResourceName,
+            bool isError)
+        {
+            await AssertWebServerSpan(
+                path,
+                _iisFixture.Agent,
+                _iisFixture.HttpPort,
+                HttpStatusCode.OK,
+                isError,
+                SpanTypes.Web,
+                "aspnet.request",
+                expectedResourceName,
+                "1.0.0");
+        }
+
+        [Fact]
+        [Trait("Category", "EndToEnd")]
+        [Trait("Integration", nameof(AspNetWebFormsTests))]
+        public async Task NestedAsyncElasticCallSubmitsTrace()
+        {
+            var testStart = DateTime.UtcNow;
+            using (var httpClient = new HttpClient())
+            {
+                // disable tracing for this HttpClient request
+                httpClient.DefaultRequestHeaders.Add(HttpHeaderNames.TracingEnabled, "false");
+
+                var response = await httpClient.GetAsync($"http://localhost:{_iisFixture.HttpPort}" + "/Database/Elasticsearch");
+                var content = await response.Content.ReadAsStringAsync();
+                Output.WriteLine($"[http] {response.StatusCode} {content}");
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
+
+            var allSpans = _iisFixture.Agent.WaitForSpans(3, minDateTime: testStart)
+                                   .OrderBy(s => s.Start)
+                                   .ToList();
+
+            Assert.True(allSpans.Count > 0, "Expected there to be spans.");
+
+            var elasticSpans = allSpans
+                             .Where(s => s.Type == "elasticsearch")
+                             .ToList();
+
+            Assert.True(elasticSpans.Count > 0, "Expected elasticsearch spans.");
+
+            foreach (var span in elasticSpans)
+            {
+                Assert.Equal("elasticsearch.query", span.Name);
+                Assert.Equal("Development Web Site-elasticsearch", span.Service);
+                Assert.Equal("elasticsearch", span.Type);
+            }
+        }
+    }
+}
+
+#endif
