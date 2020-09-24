2b99ecd8d52fce2d886cdd4b63d98c43444079b3
macrogreg <macrogreg@live.com>
2020-09-23 19:15:19 -0700

    DynamicActivityBinding: Adjust folder structure to Datadog convention.

---
+++ src/OpenTelemetry.DynamicActivityBinding/OpenTelemetry.DynamicActivityBinding/internal/DynamicInvoker.cs
@@ -0,0 +1,81 @@
+﻿using OpenTelemetry.Util;
+using System;
+using System.Reflection;
+
+namespace OpenTelemetry.DynamicActivityBinding
+{
+    internal class DynamicInvoker
+    {
+        private readonly Type _activityType;
+
+        internal DynamicInvoker(Type activityType)
+        {
+            Validate.NotNull(activityType, nameof(activityType));
+            _activityType = activityType;
+        }
+
+        public void ValidateIsActivity(object activity)
+        {
+            Validate.NotNull(activity, nameof(activity));
+
+            Type actualType = activity.GetType();
+            if (!_activityType.Equals(actualType))
+            {
+                if (_activityType.IsAssignableFrom(actualType))
+                {
+                    throw new ArgumentException($"The specified object is expected to be of type {_activityType.FullName},"
+                                              + $" but the actual runtime type is {actualType.FullName}."
+                                              + $" Notably, the expected type {_activityType.Name} is assignable from the actual runtime type {actualType.Name}."
+                                              + $" However, an exact match is required.");
+                }
+                else
+                {
+                    throw new ArgumentException($"The specified object is expected to be of type {_activityType.FullName},"
+                                              + $" but the actual runtime type is {actualType.FullName}.");
+                }
+            }
+        }
+
+        //public object Current()
+        //{
+        //    const string propertyName = "Current";
+        //    const bool isStatic = true;
+        //    const bool setMethod = false;
+
+        //    BindingFlags staticOrInstanceFlag = (isStatic ? BindingFlags.Static : BindingFlags.Instance);
+
+        //    PropertyInfo propInfo = _activityType.GetProperty(propertyName, BindingFlags.Public | staticOrInstanceFlag);
+        //    if (propInfo == null)
+        //    {
+        //        throw new InvalidOperationException($"Cannot reflect over the property \"{propertyName}\" with the BindingFlags Public and {staticOrInstanceFlag.ToString()}."
+        //                                          + $" The type being reflected is {_activityType.AssemblyQualifiedName}");
+        //    }
+
+        //    MethodInfo methodInfo = setMethod ? propInfo.SetMethod : propInfo.GetMethod;
+        //    if (methodInfo == null)
+        //    {
+        //        throw new InvalidOperationException($"Cannot obtain the {(setMethod ? "SetMethod" : "GetMethod")} for property \"{propertyName}\" via reflection."
+        //                                          + $" The type being reflected is {_activityType.AssemblyQualifiedName}");
+        //    }
+
+        //    //MulticastDelegate proxy = Delegate.CreateDelegate(typeof(Func<T, R>), methodInfo, throwOnBindFailure: true);
+        //    //proxy.I
+        //}
+
+        public void AddBaggage()
+        {
+            const string methodName = "AddBaggage";
+            const bool isStatic = true;
+
+            BindingFlags staticOrInstanceFlag = (isStatic ? BindingFlags.Static : BindingFlags.Instance);
+
+            MethodInfo methodInfo = _activityType.GetMethod(methodName, BindingFlags.Public | staticOrInstanceFlag);
+            if (methodInfo == null)
+            {
+                throw new InvalidOperationException($"Cannot reflect over the method \"{methodName}\" with the BindingFlags Public and {staticOrInstanceFlag.ToString()}."
+                                                  + $" The type being reflected is {_activityType.AssemblyQualifiedName}");
+            }
+            Type funcType = typeof(Func<,,>).MakeGenericType(_activityType, typeof(string), typeof(string));
+        }
+    }
+}
---
+++ src/OpenTelemetry.DynamicActivityBinding/OpenTelemetry.Util/internal/ArrayExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OpenTelemetry.Util
+{
+    internal static class ArrayExtensions
+    {
+        public static bool IsEqual<T>(this T[] arr1, T[] arr2) where T : IEquatable<T>
+        {
+            if (arr1 == arr2)
+            {
+                return true;
+            }
+
+            if (arr1 == null || arr2 == null || arr1.Length != arr2.Length)
+            {
+                return false;
+            }
+
+            for (int i = arr1.Length - 1; i >= 0; i--)
+            {
+                if (false == arr1[i].Equals(arr2[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
