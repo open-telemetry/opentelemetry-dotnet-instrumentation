dba0901d9db4eee755a461125c14fb575b00466b
Robert Pajak <pellared@hotmail.com>
2021-03-04 18:01:08 +0000

    Change tag key for HttpClientHandlerType for OTel

--- src/Datadog.Trace/Conventions/OtelHttpTags.cs
+++ src/Datadog.Trace/Conventions/OtelHttpTags.cs
@@ -8,7 +8,7 @@ namespace Datadog.Trace.Conventions
         protected static readonly IProperty<string>[] HttpTagsProperties =
             InstrumentationTagsProperties.Concat(
                 new Property<HttpTags, string>(Trace.Tags.HttpStatusCode, t => t.HttpStatusCode, (t, v) => t.HttpStatusCode = v),
-                new Property<HttpTags, string>("http-client-handler-type", t => t.HttpClientHandlerType, (t, v) => t.HttpClientHandlerType = v),
+                new Property<HttpTags, string>("http.client.type", t => t.HttpClientHandlerType, (t, v) => t.HttpClientHandlerType = v),
                 new Property<HttpTags, string>(Trace.Tags.HttpMethod, t => t.HttpMethod, (t, v) => t.HttpMethod = v),
                 new Property<HttpTags, string>(Trace.Tags.HttpUrl, t => t.HttpUrl, (t, v) => t.HttpUrl = v),
                 new Property<HttpTags, string>(Trace.Tags.InstrumentationName, t => t.InstrumentationName, (t, v) => t.InstrumentationName = v));
