e4feafd3c82cf352bb9815f9b38877cd6b1198c9
dszmigielski <mr.dawid.szmigielski@gmail.com>
2021-03-05 16:38:44 +0100

    Remove missed ActivityTraceId

--- src/Datadog.Trace.ServiceFabric/PropagationContext.cs
+++ src/Datadog.Trace.ServiceFabric/PropagationContext.cs
@@ -4,12 +4,12 @@ namespace Datadog.Trace.ServiceFabric
 {
     internal readonly struct PropagationContext
     {
-        public readonly ActivityTraceId TraceId;
+        public readonly TraceId TraceId;
         public readonly ulong ParentSpanId;
         public readonly SamplingPriority? SamplingPriority;
         public readonly string? Origin;
 
-        public PropagationContext(ActivityTraceId traceId, ulong parentSpanId, SamplingPriority? samplingPriority, string? origin)
+        public PropagationContext(TraceId traceId, ulong parentSpanId, SamplingPriority? samplingPriority, string? origin)
         {
             TraceId = traceId;
             ParentSpanId = parentSpanId;
