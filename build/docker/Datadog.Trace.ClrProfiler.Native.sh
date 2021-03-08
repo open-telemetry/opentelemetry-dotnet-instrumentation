52a6371872a1e1df8443dc2bf6742c86ddb45cbd
Robert Pajak <pellared@hotmail.com>
2021-03-08 16:38:28 +0000

    Refactor

--- build/docker/Datadog.Trace.ClrProfiler.Native.sh
+++ build/docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -7,16 +7,17 @@ cd "$DIR/../.."
 
 PUBLISH_OUTPUT_NET2="$( pwd )/src/bin/managed-publish/netstandard2.0"
 PUBLISH_OUTPUT_NET31="$( pwd )/src/bin/managed-publish/netcoreapp3.1"
+BUILD_TYPE=${buildConfiguration:-Debug}
 
 cd src/Datadog.Trace.ClrProfiler.Native
 mkdir -p build
-(cd build && cmake ../ -DCMAKE_BUILD_TYPE=${buildConfiguration:-Debug}  && make)
+(cd build && cmake ../ -DCMAKE_BUILD_TYPE=${BUILD_TYPE}  && make)
 
-mkdir -p bin/${buildConfiguration:-Debug}/x64
-cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/${buildConfiguration:-Debug}/x64/OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.so
+mkdir -p bin/${BUILD_TYPE}/x64
+cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/${BUILD_TYPE}/x64/OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.so
 
-mkdir -p bin/${buildConfiguration:-Debug}/x64/netstandard2.0
-cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/${buildConfiguration:-Debug}/x64/netstandard2.0/
+mkdir -p bin/${BUILD_TYPE}/x64/netstandard2.0
+cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/${BUILD_TYPE}/x64/netstandard2.0/
 
-mkdir -p bin/${buildConfiguration:-Debug}/x64/netcoreapp3.1
-cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/${buildConfiguration:-Debug}/x64/netcoreapp3.1/
+mkdir -p bin/${BUILD_TYPE}/x64/netcoreapp3.1
+cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/${BUILD_TYPE}/x64/netcoreapp3.1/
