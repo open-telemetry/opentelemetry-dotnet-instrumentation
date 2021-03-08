1d8011c8e6eaaef044d3987ea62b0c411f58ed18
Robert Pajak <pellared@hotmail.com>
2021-03-08 16:25:40 +0000

    Fix Datadog.Trace.ClrProfiler.Native.sh

--- build/docker/Datadog.Trace.ClrProfiler.Native.sh
+++ build/docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -10,13 +10,13 @@ PUBLISH_OUTPUT_NET31="$( pwd )/src/bin/managed-publish/netcoreapp3.1"
 
 cd src/Datadog.Trace.ClrProfiler.Native
 mkdir -p build
-(cd build && cmake ../ -DCMAKE_BUILD_TYPE=Release && make)
+(cd build && cmake ../ -DCMAKE_BUILD_TYPE=${buildConfiguration:-Debug}  && make)
 
-mkdir -p bin/Release/x64
-cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/Release/x64/OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.so
+mkdir -p bin/${buildConfiguration:-Debug}/x64
+cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/${buildConfiguration:-Debug}/x64/OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.so
 
-mkdir -p bin/Release/x64/netstandard2.0
-cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/Release/x64/netstandard2.0/
+mkdir -p bin/${buildConfiguration:-Debug}/x64/netstandard2.0
+cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/${buildConfiguration:-Debug}/x64/netstandard2.0/
 
-mkdir -p bin/Release/x64/netcoreapp3.1
-cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/Release/x64/netcoreapp3.1/
+mkdir -p bin/${buildConfiguration:-Debug}/x64/netcoreapp3.1
+cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/${buildConfiguration:-Debug}/x64/netcoreapp3.1/
