ed8736fa1cd301a7c526b6b16e71ad9f10d7ac3a
Robert Pajak <pellared@hotmail.com>
2021-03-11 14:59:52 +0000

    cd without output

--- dev/instrument.sh
+++ dev/instrument.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-cd $DIR/..
+cd $DIR/.. >/dev/null
 source $DIR/envvars.sh
-cd -
+cd - >/dev/null
 
 eval "$@"
