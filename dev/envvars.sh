ec21cff641967c27327c2f0fa0cf05c4d69785d0
Robert Pajak <pellared@hotmail.com>
2021-03-11 11:47:59 +0000

    Add support for .NET Framework

--- dev/envvars.sh
+++ dev/envvars.sh
@@ -23,9 +23,17 @@ native_sufix() {
 
 SUFIX=$(native_sufix)
 
+# Enable .NET Framework Profiling API
+export COR_ENABLE_PROFILING="1"
+export COR_PROFILER="{918728DD-259F-4A6A-AC2B-B85E1B658318}"
+export COR_PROFILER_PATH="${PWD}/src/Datadog.Trace.ClrProfiler.Native/bin/Debug/x64/OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.${SUFIX}"
+
+# Enable .NET Core Profiling API
 export CORECLR_ENABLE_PROFILING="1"
 export CORECLR_PROFILER="{918728DD-259F-4A6A-AC2B-B85E1B658318}"
 export CORECLR_PROFILER_PATH="${PWD}/src/Datadog.Trace.ClrProfiler.Native/bin/Debug/x64/OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.${SUFIX}"
+
+# Configure OpenTelemetry Tracer 
 export OTEL_DOTNET_TRACER_HOME="${PWD}/src/Datadog.Trace.ClrProfiler.Native/bin/Debug/x64"
 export OTEL_INTEGRATIONS="${PWD}/integrations.json"
 export OTEL_VERSION="1.0.0"
