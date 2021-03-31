e0a520ace02ff9bf8f1090650b4672b557a4446a
Rasmus Kuusmann <rkuusmann@splunk.com>
2021-03-31 20:42:40 +0300

    Improve PropagationExtensions

--- src/Datadog.Trace/Propagation/PropagationExtensions.cs
+++ src/Datadog.Trace/Propagation/PropagationExtensions.cs
@@ -7,12 +7,12 @@ namespace Datadog.Trace.Propagation
     {
         public static void Inject(this IPropagator propagator, SpanContext context, IHeadersCollection headers)
         {
-            propagator.Inject(context, headers, (carrier, header, value) => carrier.Set(header, value));
+            propagator.Inject(context, headers, InjectToHeadersCollection);
         }
 
         public static SpanContext Extract(this IPropagator propagator, IHeadersCollection headers)
         {
-            return propagator.Extract(headers, (carrier, header) => carrier.GetValues(header));
+            return propagator.Extract(headers, ExtractFromHeadersCollection);
         }
 
         public static IEnumerable<KeyValuePair<string, string>> ExtractHeaderTags(this IHeadersCollection headers, IEnumerable<KeyValuePair<string, string>> headerToTagMap)
@@ -31,6 +31,16 @@ namespace Datadog.Trace.Propagation
         public static string ParseString(this IHeadersCollection headers, string headerName)
         {
             return PropagationHelpers.ParseString(headers, (carrier, header) => carrier.GetValues(header), headerName);
+        }
+
+        private static void InjectToHeadersCollection(IHeadersCollection carrier, string header, string value)
+        {
+            carrier.Set(header, value);
+        }
+
+        private static IEnumerable<string> ExtractFromHeadersCollection(IHeadersCollection carrier, string header)
+        {
+            return carrier.GetValues(header);
         }
     }
 }
