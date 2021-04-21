bb2cd02a9bd49d7a0ad833f6486671cf6c0b171b
dszmigielski <mr.dawid.szmigielski@gmail.com>
2021-04-21 11:28:40 +0200

    Add workflow for storing artifacts

--- build/docker/package.sh
+++ build/docker/package.sh
@@ -3,9 +3,12 @@ set -euxo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 VERSION=1.24.0
+BUILD_TYPE=${buildConfiguration:-Debug}
 
 mkdir -p $DIR/../../deploy/linux
-cp $DIR/../../integrations.json $DIR/../../src/Datadog.Trace.ClrProfiler.Native/bin/${BUILD_TYPE}/x64/
+for target in integrations.json defaults.env LICENSE NOTICE ; do
+    cp $DIR/../../$target $DIR/../../src/Datadog.Trace.ClrProfiler.Native/bin/${BUILD_TYPE}/x64/
+done
 cp $DIR/../../build/artifacts/createLogPath.sh $DIR/../../src/Datadog.Trace.ClrProfiler.Native/bin/${BUILD_TYPE}/x64/
 
 cd $DIR/../../deploy/linux
