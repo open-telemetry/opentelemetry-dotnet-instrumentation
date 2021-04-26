8ad9575c0eb97c2287b58223b943c4abb703a9f1
dszmigielski <mr.dawid.szmigielski@gmail.com>
2021-04-26 08:02:33 +0200

    Remove unnecessary lines from build.cmd; add rem and local env vars

--- build/build.cmd
+++ build/build.cmd
@@ -1,12 +1,22 @@
+rem To run this script locally you need the following tools to be added the path: nuget, msbuild, dotnet. 
+
+setlocal
+
 IF "%buildConfiguration%"=="" (SET configuration=Debug) ELSE (SET configuration=%buildConfiguration%)
 
 nuget restore Datadog.Trace.sln
+if %errorlevel% neq 0 exit /b %errorlevel%
 nuget restore src\OpenTelemetry.DynamicActivityBinding\OpenTelemetry.DynamicActivityBinding.sln
+if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild Datadog.Trace.proj /t:BuildCsharp /p:Configuration=%configuration%
-dotnet pack -c %configuration% -o src\bin\SignalFx.Tracing src\Datadog.Trace\Datadog.Trace.csproj
-dotnet pack -c %configuration% -o src\bin\SignalFx.Tracing.OpenTracing src\Datadog.Trace.OpenTracing\Datadog.Trace.OpenTracing.csproj
+if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild Datadog.Trace.proj /t:BuildCpp /p:Configuration=%configuration%;Platform=x64
+if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild Datadog.Trace.proj /t:BuildCpp /p:Configuration=%configuration%;Platform=x86
+if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild Datadog.Trace.proj /t:msi /p:Configuration=%configuration%;Platform=x64
+if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild Datadog.Trace.proj /t:msi /p:Configuration=%configuration%;Platform=x86
+if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild Datadog.Trace.proj /t:CreateHomeDirectory /p:Configuration=%configuration%;Platform=x64
+if %errorlevel% neq 0 exit /b %errorlevel%
