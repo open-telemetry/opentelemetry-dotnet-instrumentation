e34e3c9b41e7608fea19455d9988cd9b9d877c1f
Paulo Janotti <pjanotti@splunk.com>
2021-06-01 12:41:42 -0700

    PoC: Add support to testing OpenTracing integration (#186)
    * Add OpenTracingShim to SDK initialization
    
    * Add OpenTracingShim as option to ConsoleApp
    
    * Change wrapper method to async
    
    * Match casing of other poc variables
    
    * Change log message to "tracer initialized"
    
    * Make OT span default on ConsoleApp
    
    * Fixes .vcxproj file for latest msbuild version. (#1495)
    
    Co-authored-by: Tony Redondo <tony.redondo@datadoghq.com>

---
+++ samples/OpenTracingLibrary/Wrapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OpenTracing.Util;
+
+namespace OpenTracingLibrary
+{
+    public static class Wrapper
+    {
+        public static async Task WithOpenTracingSpanAsync(string spanKind, Func<Task> wrapped)
+        {
+            var tracer = GlobalTracer.Instance;
+            Console.WriteLine($">>>>>>>>>>>>>>>>>>>>>>> OpenTracing.{tracer}");
+            using (var scope = tracer.BuildSpan("OpenTracing Span")
+                .WithTag("span.kind", spanKind)
+                .StartActive())
+            {
+                try
+                {
+                    await wrapped();
+                    scope.Span.Log("action success");
+                    scope.Span.SetTag("action.success", true);
+                }
+                catch (Exception ex)
+                {
+                    scope.Span.SetTag("error", true);
+                    var eventData = new Dictionary<string, object>
+                    {
+                        { "event", "error" },
+                        { "error.kind", "Exception" },
+                        { "error.object", ex },
+                        { "stack", ex.StackTrace.ToString() },
+                    };
+                    scope.Span.Log(eventData);
+                    throw;
+                }
+            }
+        }
+    }
+}
