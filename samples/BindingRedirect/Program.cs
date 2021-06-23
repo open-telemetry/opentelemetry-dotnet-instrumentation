d455c5043d16d739fe9612606cc3a64c739319c8
dszmigielski <dszmigielski@splunk.com>
2021-06-23 19:12:53 +0200

    Add binding redirection sample app

---
+++ samples/BindingRedirect/Program.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using OldReference;
+
+namespace BindingRedirect
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await InstrumentedHttpCall.Get("https://www.google.com");
+
+        }
+    }
+}
