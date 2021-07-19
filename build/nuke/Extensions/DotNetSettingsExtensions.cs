f8927ab7f72c38ff6a88e2a98cbaba4b03951792
RassK <rkuusmann@splunk.com>
2021-07-19 19:53:26 +0300

    Cleanup unused methods

--- build/nuke/Extensions/DotNetSettingsExtensions.cs
+++ build/nuke/Extensions/DotNetSettingsExtensions.cs
@@ -3,22 +3,12 @@ using Nuke.Common.Tools.MSBuild;
 
 internal static class DotNetSettingsExtensions
 {
-    public static DotNetBuildSettings SetTargetPlatformAnyCPU(this DotNetBuildSettings settings)
-        => settings.SetTargetPlatform(MSBuildTargetPlatform.MSIL);
-
     public static DotNetPublishSettings SetTargetPlatformAnyCPU(this DotNetPublishSettings settings)
         => settings.SetTargetPlatform(MSBuildTargetPlatform.MSIL);
 
     public static T SetTargetPlatformAnyCPU<T>(this T settings)
         where T : MSBuildSettings
         => settings.SetTargetPlatform(MSBuildTargetPlatform.MSIL);
-
-    public static DotNetBuildSettings SetTargetPlatform(this DotNetBuildSettings settings, MSBuildTargetPlatform platform)
-    {
-        return platform is null
-            ? settings
-            : settings.SetProperty("Platform", GetTargetPlatform(platform));
-    }
 
     public static DotNetPublishSettings SetTargetPlatform(this DotNetPublishSettings settings, MSBuildTargetPlatform platform)
     {
