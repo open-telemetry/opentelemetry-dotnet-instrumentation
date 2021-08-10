e67f8657954f1a34fd21ea690803702c06c598e0
Paulo Janotti <pjanotti@splunk.com>
2021-08-10 07:25:39 -0700

    [PoC] Document DevOps proposed workarounds (#210)
    * Add .NET Core App using OldReference
    
    * Document the devops scenario workarounds
    
    * Fix sample app links
    
    * PR feedback
    
    Co-authored-by: Robert Pająk <rpajak@splunk.com>

---
+++ samples/CoreAppOldReference/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using OldReference;
+
+namespace BindingRedirect
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await InstrumentedHttpCall.GetAsync("https://www.google.com");
+        }
+    }
+}
