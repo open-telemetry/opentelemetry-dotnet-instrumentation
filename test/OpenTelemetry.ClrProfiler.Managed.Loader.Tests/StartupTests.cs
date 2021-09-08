66d093a4377982373afe366f7a9ec1c2eae2be00
dszmigielski <dszmigielski@splunk.com>
2021-09-08 13:56:22 +0200

    Add checking loaded assemblies to the test

--- test/OpenTelemetry.ClrProfiler.Managed.Loader.Tests/StartupTests.cs
+++ test/OpenTelemetry.ClrProfiler.Managed.Loader.Tests/StartupTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace OpenTelemetry.ClrProfiler.Managed.Loader.Tests
@@ -15,6 +16,11 @@ namespace OpenTelemetry.ClrProfiler.Managed.Loader.Tests
             var exception = Record.Exception(() => Startup.ManagedProfilerDirectory);
             // That means the assembly was loaded successfully and Initialize method was called.
             Assert.Null(exception);
+
+            var clrProfilerManagedAssembly = AppDomain.CurrentDomain.GetAssemblies()
+                .Select(a => a.FullName)
+                .FirstOrDefault(n => n.StartsWith("OpenTelemetry.ClrProfiler.Managed,"));
+            Assert.NotNull(clrProfilerManagedAssembly);
         }
     }
 }
