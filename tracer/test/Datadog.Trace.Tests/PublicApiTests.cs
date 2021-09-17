da25ea0c41db143c5272f6cc721a385b5bbc8e89
Paulo Janotti <pjanotti@splunk.com>
2021-09-17 08:53:41 -0700

    Skip Public API test

--- tracer/test/Datadog.Trace.Tests/PublicApiTests.cs
+++ tracer/test/Datadog.Trace.Tests/PublicApiTests.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="PublicApiTests.cs" company="Datadog">
+// <copyright file="PublicApiTests.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -24,7 +24,7 @@ namespace Datadog.Trace.Tests
             ExcludeAttributes = new[] { typeof(InternalsVisibleToAttribute).FullName, },
         };
 
-        [Fact]
+        [Fact(Skip = "OTel doesn't have a published public API")]
         public void PublicApiHasNotChanged()
         {
             var assembly = typeof(Tracer).Assembly;
