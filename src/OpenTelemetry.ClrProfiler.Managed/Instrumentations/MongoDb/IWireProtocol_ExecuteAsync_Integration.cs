4ebaf69e8c568b9c7a63d50dc9f29a74d42c49b3
RassK <rkuusmann@splunk.com>
2021-09-21 14:14:42 +0300

    Update mongodb integrations

--- src/OpenTelemetry.ClrProfiler.Managed/Instrumentations/MongoDb/IWireProtocol_ExecuteAsync_Integration.cs
+++ src/OpenTelemetry.ClrProfiler.Managed/Instrumentations/MongoDb/IWireProtocol_ExecuteAsync_Integration.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Threading;
 using OpenTelemetry.ClrProfiler.CallTarget;
 using OpenTelemetry.ClrProfiler.Managed.Util;
@@ -30,6 +31,8 @@ namespace OpenTelemetry.ClrProfiler.Managed.Instrumentations.MongoDb
         typeName: "MongoDB.Driver.Core.WireProtocol.KillCursorsWireProtocol",
         isGeneric: false)]
     // ReSharper disable once InconsistentNaming
+    [Browsable(false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public class IWireProtocol_ExecuteAsync_Integration
     {
         /// <summary>
