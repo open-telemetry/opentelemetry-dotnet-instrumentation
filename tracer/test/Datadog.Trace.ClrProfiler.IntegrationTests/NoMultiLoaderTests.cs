64429352766896ee634ca14c38b827b7e2c22102
Paulo Janotti <pjanotti@splunk.com>
2021-09-21 20:45:25 -0700

    Count correct module on SingleLoaderTest

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/NoMultiLoaderTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/NoMultiLoaderTests.cs
@@ -29,7 +29,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             SetEnvironmentVariable("OTEL_TRACE_LOG_PATH", tmpFile);
             using ProcessResult processResult = RunSampleAndWaitForExit(9696);
             string[] logFileContent = File.ReadAllLines(tmpFile);
-            int numOfLoadersLoad = logFileContent.Count(line => line.Contains("Datadog.Trace.ClrProfiler.Managed.Loader loaded"));
+            int numOfLoadersLoad = logFileContent.Count(line => line.Contains("OpenTelemetry.AutoInstrumentation.ClrProfiler.Managed.Loader loaded"));
             Assert.Equal(1, numOfLoadersLoad);
         }
     }
