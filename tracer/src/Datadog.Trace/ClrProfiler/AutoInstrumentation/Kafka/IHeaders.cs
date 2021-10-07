f80558fb28a1815d3a62db8fda1ec3795f277598
Paulo Janotti <pjanotti@splunk.com>
2021-10-06 17:47:12 -0700

    Revert "Remove copyright headers"
    This reverts commit beeb9ebe568e3165af28dd63f4034acad1a7cb36.
    
    Per conversation at
    https://github.com/open-telemetry/opentelemetry-dotnet-instrumentation/pull/188#issuecomment-857725818
    the SIG actually planned to do it later and since we will switch the
    branches soon there is no need to do it now.

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Kafka/IHeaders.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Kafka/IHeaders.cs
@@ -1,4 +1,9 @@
-﻿using System.ComponentModel;
+﻿// <copyright file="IHeaders.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.ComponentModel;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Kafka
 {
--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/ILoggerTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/ILoggerTests.cs
@@ -1,4 +1,9 @@
-﻿#if !NET452
+﻿// <copyright file="ILoggerTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#if !NET452
 using System;
 using Datadog.Trace.TestHelpers;
 using FluentAssertions;
