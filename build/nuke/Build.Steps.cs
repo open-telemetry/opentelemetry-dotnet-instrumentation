1ab7a5fd906406595fb894d28bab9ab331f16979
Rajkumar Rangaraj <rajrang@microsoft.com>
2021-12-07 12:34:04 -0800

    Fix AdditionalDeps path

--- build/nuke/Build.Steps.cs
+++ build/nuke/Build.Steps.cs
@@ -30,7 +30,7 @@ partial class Build
     AbsolutePath ArtifactsDirectory => Artifacts ?? (OutputDirectory / "artifacts");
     AbsolutePath BuildDataDirectory => RootDirectory / "build_data";
     AbsolutePath ProfilerTestLogs => BuildDataDirectory / "profiler-logs";
-    AbsolutePath AdditionalDepsDirectory => TracerHomeDirectory / "additionalDeps";
+    AbsolutePath AdditionalDepsDirectory => TracerHomeDirectory / "AdditionalDeps";
 
     Project NativeProfilerProject => Solution.GetProject(Projects.ClrProfilerNative);
 
