e3de6d2d40b3f40827d44613d4b58a5edde46897
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-01-21 07:14:25 +0100

    handle exception instead of using wokring endpoints

--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Diagnostics;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTracing.Propagation;
@@ -34,13 +35,14 @@ namespace ConsoleApp
                 activity?.SetTag("foo", "bar");
 
                 await HttpGet("https://www.example.com/default-handler");
-                await HttpGet("https://www.example.com/test-handler");
-                await HttpGet("https://www.example.com/another-handler");
+                await HttpGet("http://127.0.0.1:8080/api/mongo");
+                await HttpGet("http://127.0.0.1:8080/api/redis");
             }
-            
+
+            const string requestUrl = "https://www.example.com/";
             var request = new HttpRequestMessage
             {
-                RequestUri = new Uri("https://www.example.com/"),
+                RequestUri = new Uri(requestUrl),
                 Method = HttpMethod.Post,
                 Content = new StringContent(string.Empty, Encoding.UTF8),
             };
@@ -58,21 +60,56 @@ namespace ConsoleApp
                     request.Headers.Add(kvp.Key, kvp.Value);
                 }
 
-                using var response = await client.SendAsync(request);
+                int? statusCode;
+                int? responseLength = null;
+                try
+                {
+                    using var response = await client.SendAsync(request);
 
-                scope.Span.SetTag("http.status_code", (int)response.StatusCode);
+                    statusCode = (int) response.StatusCode;
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    responseLength = responseContent.Length;
+                }
+                catch (HttpRequestException e)
+                {
+                    LogHttpRequestException(requestUrl, e);
+#if NET5_0_OR_GREATER
+                    statusCode = (int?) e.StatusCode;
+#else
+                    statusCode = null;
+#endif
+                }
 
-                var responseContent = await response.Content.ReadAsStringAsync();
-                scope.Span.SetTag("response.length", responseContent.Length);
+                if (statusCode.HasValue)
+                {
+                    scope.Span.SetTag("http.status_code", statusCode.Value);
+                }
+
+                if (responseLength.HasValue)
+                {
+                    scope.Span.SetTag("response.length", responseLength.Value);
+                }
             }
+        }
+
+        private static void LogHttpRequestException(string requestUrl, HttpRequestException httpRequestException)
+        {
+            Console.WriteLine($"HttpRequestException occurred while calling {requestUrl}, {httpRequestException}");
         }
 
         private static async Task HttpGet(string url)
         {
-            using var client = new HttpClient();
-            Console.WriteLine($"Calling {url}");
-            await client.GetAsync(url);
-            Console.WriteLine($"Called {url}");
+            try
+            {
+                using var client = new HttpClient();
+                Console.WriteLine($"Calling {url}");
+                await client.GetAsync(url);
+                Console.WriteLine($"Called {url}");
+            }
+            catch (HttpRequestException e)
+            {
+                LogHttpRequestException(url, e);
+            }
         }
     }
 }
