f6192b74e4f5df3897181ec8ac2b43e203951264
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-01-31 13:32:02 +0100

    remove redundant usings

--- src/OpenTelemetry.ClrProfiler.Managed/Configuration/EnvironmentConfigurationHelper.cs
+++ src/OpenTelemetry.ClrProfiler.Managed/Configuration/EnvironmentConfigurationHelper.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
