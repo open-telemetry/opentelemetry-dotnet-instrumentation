23d95a3d9afa4a4cfbb027c6619621a90cddd724
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-02-09 06:58:42 +0100

    add empty OpenTelemetry.ClrProfiler.Managed.Tests

---
+++ test/OpenTelemetry.ClrProfiler.Managed.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace OpenTelemetry.ClrProfiler.Managed.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
