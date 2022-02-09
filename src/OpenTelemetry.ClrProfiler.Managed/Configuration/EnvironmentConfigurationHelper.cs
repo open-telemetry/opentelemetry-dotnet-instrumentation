e2ada19ffc671f768ad2c507e710f340ff501691
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-02-09 19:01:43 +0100

    Update src/OpenTelemetry.ClrProfiler.Managed/Configuration/EnvironmentConfigurationHelper.cs
    Co-authored-by: Zach Montoya <zach.montoya@datadoghq.com>

--- src/OpenTelemetry.ClrProfiler.Managed/Configuration/EnvironmentConfigurationHelper.cs
+++ src/OpenTelemetry.ClrProfiler.Managed/Configuration/EnvironmentConfigurationHelper.cs
@@ -96,7 +96,7 @@ namespace OpenTelemetry.ClrProfiler.Managed.Configuration
                 case null:
                     break;
                 default:
-                    throw new FormatException($"The exporter name '{settings}' is not recognized");
+                    throw new FormatException($"The exporter name '{settings.Exporter}' is not recognized");
             }
 
             return builder;
