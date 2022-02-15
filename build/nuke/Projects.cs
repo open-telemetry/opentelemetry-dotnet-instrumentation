17b351e749c8fa7c4cfbde074cb207e9a05fdd6c
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-02-15 10:29:16 +0100

    part 8

--- build/nuke/Projects.cs
+++ build/nuke/Projects.cs
@@ -1,24 +1,24 @@
 public static class Projects
 {
-    public const string ClrProfilerManaged = "OpenTelemetry.AutoInstrumentation";
-    public const string ClrProfilerManagedCore = "OpenTelemetry.AutoInstrumentation.Core";
-    public const string ClrProfilerManagedLoader = "OpenTelemetry.AutoInstrumentation.Loader";
-    public const string ClrProfilerNative = "OpenTelemetry.AutoInstrumentation.Native";
-    public const string StartupHook = "OpenTelemetry.AutoInstrumentation.StartupHook";
-    public const string AdditionalDeps = "OpenTelemetry.AutoInstrumentation.AdditionalDeps";
+    public const string AutoInstrumentation = "OpenTelemetry.AutoInstrumentation";
+    public const string AutoInstrumentationCore = "OpenTelemetry.AutoInstrumentation.Core";
+    public const string AutoInstrumentationLoader = "OpenTelemetry.AutoInstrumentation.Loader";
+    public const string AutoInstrumentationNative = "OpenTelemetry.AutoInstrumentation.Native";
+    public const string AutoInstrumentationStartupHook = "OpenTelemetry.AutoInstrumentation.AutoInstrumentationStartupHook";
+    public const string AutoInstrumentationAdditionalDeps = "OpenTelemetry.AutoInstrumentation.AutoInstrumentationAdditionalDeps";
 
     public static class Tests
     {
-        public const string ClrProfilerNativeTests = "OpenTelemetry.AutoInstrumentation.Tests";
-        public const string ClrProfilerManagedLoaderTests = "OpenTelemetry.AutoInstrumentation.Loader.Tests";
-        public const string ClrProfilerManagedBootstrappingTests = "OpenTelemetry.AutoInstrumentation.Bootstrapping.Tests";
-        public const string ClrProfilerManagedTests = "OpenTelemetry.AutoInstrumentation.Tests";
+        public const string AutoInstrumentationNativeTests = "OpenTelemetry.AutoInstrumentation.Tests";
+        public const string AutoInstrumentationLoaderTests = "OpenTelemetry.AutoInstrumentation.Loader.Tests";
+        public const string AutoInstrumentationBootstrappingTests = "OpenTelemetry.AutoInstrumentation.Bootstrapping.Tests";
+        public const string AutoInstrumentationTests = "OpenTelemetry.AutoInstrumentation.Tests";
 
         public const string IntegrationTestsHelpers = "IntegrationTests.Helpers";
     }
 
     public static class Mocks
     {
-        public const string ClrProfilerManagedMock = "OpenTelemetry.AutoInstrumentation.Mock";
+        public const string AutoInstrumentationMock = "OpenTelemetry.AutoInstrumentation.Mock";
     }
 }
