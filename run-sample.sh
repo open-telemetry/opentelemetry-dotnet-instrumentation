c56ebd11d1693501214bfe5ee56b405aea8d1bc6
Paulo Janotti <pjanotti@splunk.com>
2022-03-15 13:51:04 -0700

    Make exporter type configurable on run-sample.sh

--- run-sample.sh
+++ run-sample.sh
@@ -8,6 +8,9 @@ aspNetAppTargetFramework=${aspNetAppTargetFramework:-netcoreapp3.1}
 sampleAppTargetFramework=${sampleAppTargetFramework:-netcoreapp3.1}
 sampleApp=${sampleApp:-ConsoleApp}
 
+# Defaults for select OTEL env vars
+exporter=${exporter:-jaeger}
+
 function finish {
   docker stop jaeger # stop Jaeger
   docker stop redis # stop Redis
@@ -44,12 +47,12 @@ docker run -d --rm --name jaeger \
 
 # instrument and run HTTP server app in background
 export OTEL_DOTNET_AUTO_INSTRUMENTATION_PLUGINS="Samples.AspNetCoreMvc.OtelSdkPlugin, Samples.AspNetCoreMvc31, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null:Vendor.Distro.Plugin, Vendor.Distro, Version=0.0.1.0, Culture=neutral, PublicKeyToken=null"
-./dev/instrument.sh OTEL_DOTNET_AUTO_ENABLED_INSTRUMENTATIONS="AspNet,SqlClient,MongoDb" OTEL_SERVICE_NAME="aspnet-server" OTEL_TRACES_EXPORTER="jaeger" ASPNETCORE_URLS="http://127.0.0.1:8080/" dotnet run --no-launch-profile -f $aspNetAppTargetFramework --project ./samples/Samples.AspNetCoreMvc31/Samples.AspNetCoreMvc31.csproj &
+./dev/instrument.sh OTEL_DOTNET_AUTO_ENABLED_INSTRUMENTATIONS="AspNet,SqlClient,MongoDb" OTEL_SERVICE_NAME="aspnet-server" OTEL_TRACES_EXPORTER=${exporter} ASPNETCORE_URLS="http://127.0.0.1:8080/" dotnet run --no-launch-profile -f $aspNetAppTargetFramework --project ./samples/Samples.AspNetCoreMvc31/Samples.AspNetCoreMvc31.csproj &
 unset OTEL_DOTNET_AUTO_INSTRUMENTATION_PLUGINS
 ./dev/wait-local-port.sh 8080
 
 # instrument and run HTTP client app
-time ./dev/instrument.sh OTEL_DOTNET_AUTO_ENABLED_INSTRUMENTATIONS="HttpClient" OTEL_SERVICE_NAME="http-client" OTEL_TRACES_EXPORTER="jaeger" dotnet run --no-launch-profile -f $sampleAppTargetFramework --project ./samples/${sampleApp}/${sampleApp}.csproj
+time ./dev/instrument.sh OTEL_DOTNET_AUTO_ENABLED_INSTRUMENTATIONS="HttpClient" OTEL_SERVICE_NAME="http-client" OTEL_TRACES_EXPORTER=${exporter} dotnet run --no-launch-profile -f $sampleAppTargetFramework --project ./samples/${sampleApp}/${sampleApp}.csproj
 
 # verify if it works
 read -p "Check traces under: http://localhost:16686/search. Press enter to close containers and stop sample apps"
