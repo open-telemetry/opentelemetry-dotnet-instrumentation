f6a9c34f147145fda80318d95eb6db79a030bb10
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-03-17 08:34:06 +0100

    fix SA1210

--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -16,8 +16,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTracing.Propagation;
