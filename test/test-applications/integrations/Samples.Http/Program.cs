35bf5c3aa9d538962ae0120580e0bfbda0afe1e3
dszmigielski <dszmigielski@splunk.com>
2022-04-12 09:50:11 +0200

    Apply minor fixes

--- test/test-applications/integrations/Samples.Http/Program.cs
+++ test/test-applications/integrations/Samples.Http/Program.cs
@@ -34,7 +34,6 @@ namespace Samples.Http
 
         public static IHostBuilder CreateHostBuilder(string[] args, int port) =>
             Host.CreateDefaultBuilder(args)
-
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
