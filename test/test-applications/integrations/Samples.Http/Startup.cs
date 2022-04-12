dbacce7cb8a87c7d6263bd7944e5edebd5da7200
Dawid Szmigielski <dszmigielski@splunk.com>
2022-04-12 18:10:15 +0200

    HttpClient and ASP.NET tests (#461)
    * Add Http tests
    
    * Rename Fixture
    
    * Fix project path
    
    * Apply fixes after code review
    
    * Fix Program to avoid callng endpoint before the server inits
    
    * Change tests to use fluent assertions
    
    * Remove code duplication
    
    * Apply minor fixes
    
    Co-authored-by: Robert Pająk <pellared@hotmail.com>
    Co-authored-by: Paulo Janotti <pjanotti@splunk.com>

---
+++ test/test-applications/integrations/Samples.Http/Startup.cs
@@ -0,0 +1,58 @@
+// <copyright file="Startup.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Samples.Http
+{
+    public class Startup
+    {
+        private static readonly ActivitySource MyActivitySource = new ActivitySource("Samples.Http", "1.0.0");
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.Map(
+                "/test",
+                configuration => configuration.Run(async context =>
+                {
+                    using (var activity = MyActivitySource.StartActivity("manual span"))
+                    {
+                        activity?.SetTag("test_tag", "test_value");
+                    }
+
+                    await context.Response.WriteAsync("Pong");
+                }));
+        }
+    }
+}
