bd23086ccd05f4d4bb917271989f5d665a4d8e8f
Robert Pająk <pellared@hotmail.com>
2022-06-27 12:53:24 +0200

    Refine StartupHookTests

--- test/test-applications/integrations/TestApplication.StartupHook/Program.cs
+++ test/test-applications/integrations/TestApplication.StartupHook/Program.cs
@@ -1,13 +1,29 @@
+// <copyright file="Program.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
 using System.Diagnostics;
 using System.Net.Http;
 
 namespace TestApplication.StartupHook
 {
-    class Program
+    public class Program
     {
         private static readonly ActivitySource MyActivitySource = new ActivitySource("TestApplication.StartupHook", "1.0.0");
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             using (var activity = MyActivitySource.StartActivity("SayHello"))
             {
