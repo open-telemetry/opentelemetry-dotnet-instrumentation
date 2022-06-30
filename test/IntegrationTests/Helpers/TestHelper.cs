eae6e7925019b1569cd93c6aa292a42ad4490ccd
Rajkumar Rangaraj <rajrang@microsoft.com>
2022-06-30 13:49:53 -0700

    PR feedback

--- test/IntegrationTests/Helpers/TestHelper.cs
+++ test/IntegrationTests/Helpers/TestHelper.cs
@@ -92,6 +92,7 @@ public abstract class TestHelper
 
         if (testSettings.MetricsSettings != null)
         {
+            Output.WriteLine($"Otlp Endpoint: {agentBaseUrl}");
             builder = builder.WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", agentBaseUrl);
             builder = builder.WithEnvironment("OTEL_METRIC_EXPORT_INTERVAL", "1000");
             builder = builder.WithEnvironment("OTEL_DOTNET_AUTO_METRICS_ENABLED_INSTRUMENTATIONS", "AspNet");
