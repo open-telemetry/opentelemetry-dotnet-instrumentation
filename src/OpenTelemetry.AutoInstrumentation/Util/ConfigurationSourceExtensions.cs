1aaaf328ff9ed912bc17b50a155c826f2a8017b9
RassK <rkuusmann@splunk.com>
2022-07-15 02:22:52 +0300

    add none option

---
+++ src/OpenTelemetry.AutoInstrumentation/Util/ConfigurationSourceExtensions.cs
@@ -0,0 +1,73 @@
+// <copyright file="ConfigurationSourceExtensions.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenTelemetry.AutoInstrumentation.Configuration;
+
+namespace OpenTelemetry.AutoInstrumentation.Util
+{
+    internal static class ConfigurationSourceExtensions
+    {
+        private const string None = "none";
+
+        public static IList<TEnum> ParseEnabledEnumList<TEnum>(this IConfigurationSource source, string enabledConfiguration, string disabledConfiguration, char separator, string error)
+            where TEnum : struct, IConvertible
+        {
+            var instrumentations = new Dictionary<string, TEnum>();
+            var enabledInstrumentations = source.GetString(enabledConfiguration);
+            if (enabledInstrumentations != null)
+            {
+                if (enabledInstrumentations == None)
+                {
+                    return Array.Empty<TEnum>();
+                }
+
+                foreach (var instrumentation in enabledInstrumentations.Split(separator))
+                {
+                    if (Enum.TryParse(instrumentation, out TEnum parsedType))
+                    {
+                        instrumentations[instrumentation] = parsedType;
+                    }
+                    else
+                    {
+                        throw new FormatException(string.Format(error, instrumentation));
+                    }
+                }
+            }
+            else
+            {
+                instrumentations = Enum.GetValues(typeof(TEnum))
+                    .Cast<TEnum>()
+                    .ToDictionary(
+                        key => Enum.GetName(typeof(TEnum), key),
+                        val => val);
+            }
+
+            var disabledInstrumentations = source.GetString(disabledConfiguration);
+            if (disabledInstrumentations != null)
+            {
+                foreach (var instrumentation in disabledInstrumentations.Split(separator))
+                {
+                    instrumentations.Remove(instrumentation);
+                }
+            }
+
+            return instrumentations.Values.ToList();
+        }
+    }
+}
---
+++ test/OpenTelemetry.AutoInstrumentation.Tests/Util/ConfigurationSourceExtensionsTests.cs
@@ -0,0 +1,126 @@
+// <copyright file="ConfigurationSourceExtensionsTests.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Specialized;
+using FluentAssertions;
+using OpenTelemetry.AutoInstrumentation.Configuration;
+using OpenTelemetry.AutoInstrumentation.Util;
+using Xunit;
+
+namespace OpenTelemetry.AutoInstrumentation.Tests.Util
+{
+    public class ConfigurationSourceExtensionsTests
+    {
+        private enum TestEnum
+        {
+            Test1,
+            Test2,
+            Test3
+        }
+
+        [Fact]
+        public void ParseEnabledEnumList_Default()
+        {
+            var source = new NameValueConfigurationSource(new NameValueCollection());
+
+            var list = source.ParseEnabledEnumList<TestEnum>(
+                enabledConfiguration: "TEST_ENABLED_VALUES",
+                disabledConfiguration: "TEST_DISABLED_VALUES",
+                separator: ',',
+                error: "Invalid enum value: {0}");
+
+            list.Should().NotBeEmpty()
+                .And.HaveCount(3)
+                .And.ContainInOrder(new[] { TestEnum.Test1, TestEnum.Test2, TestEnum.Test3 })
+                .And.ContainItemsAssignableTo<TestEnum>();
+        }
+
+        [Fact]
+        public void ParseEnabledEnumList_Enabled()
+        {
+            var source = new NameValueConfigurationSource(new NameValueCollection()
+            {
+                { "TEST_ENABLED_VALUES", "Test1,Test3" }
+            });
+
+            var list = source.ParseEnabledEnumList<TestEnum>(
+                enabledConfiguration: "TEST_ENABLED_VALUES",
+                disabledConfiguration: "TEST_DISABLED_VALUES",
+                separator: ',',
+                error: "Invalid enum value: {0}");
+
+            list.Should().NotBeEmpty()
+                .And.HaveCount(2)
+                .And.ContainInOrder(new[] { TestEnum.Test1, TestEnum.Test3 })
+                .And.ContainItemsAssignableTo<TestEnum>();
+        }
+
+        [Fact]
+        public void ParseEnabledEnumList_Disabled()
+        {
+            var source = new NameValueConfigurationSource(new NameValueCollection()
+            {
+                { "TEST_DISABLED_VALUES", "Test2" }
+            });
+
+            var list = source.ParseEnabledEnumList<TestEnum>(
+                enabledConfiguration: "TEST_ENABLED_VALUES",
+                disabledConfiguration: "TEST_DISABLED_VALUES",
+                separator: ',',
+                error: "Invalid enum value: {0}");
+
+            list.Should().NotBeEmpty()
+                .And.HaveCount(2)
+                .And.ContainInOrder(new[] { TestEnum.Test1, TestEnum.Test3 })
+                .And.ContainItemsAssignableTo<TestEnum>();
+        }
+
+        [Fact]
+        public void ParseEnabledEnumList_None()
+        {
+            var source = new NameValueConfigurationSource(new NameValueCollection()
+            {
+                { "TEST_ENABLED_VALUES", "none" }
+            });
+
+            var list = source.ParseEnabledEnumList<TestEnum>(
+                enabledConfiguration: "TEST_ENABLED_VALUES",
+                disabledConfiguration: "TEST_DISABLED_VALUES",
+                separator: ',',
+                error: "Invalid enum value: {0}");
+
+            list.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ParseEnabledEnumList_Invalid()
+        {
+            var source = new NameValueConfigurationSource(new NameValueCollection()
+            {
+                { "TEST_ENABLED_VALUES", "invalid" }
+            });
+
+            source.Invoking(s => s.ParseEnabledEnumList<TestEnum>(
+                enabledConfiguration: "TEST_ENABLED_VALUES",
+                disabledConfiguration: "TEST_DISABLED_VALUES",
+                separator: ',',
+                error: "Invalid enum value: {0}"))
+                .Should().Throw<FormatException>()
+                .WithMessage("Invalid enum value: invalid");
+        }
+    }
+}
