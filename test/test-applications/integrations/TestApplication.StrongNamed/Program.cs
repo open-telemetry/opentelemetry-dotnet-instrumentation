d5d695636cd037271d9fdda9678ad138d63d3426
Paulo Janotti <pjanotti@splunk.com>
2022-09-02 14:06:54 -0700

    dotnet-format fixes for new cs code

--- test/test-applications/integrations/TestApplication.StrongNamed/Program.cs
+++ test/test-applications/integrations/TestApplication.StrongNamed/Program.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Program.cs" company="OpenTelemetry Authors">
+// <copyright file="Program.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,7 +17,7 @@
 using System;
 using System.Reflection;
 
-[assembly:AssemblyKeyFileAttribute("keypair.snk")]
+[assembly: AssemblyKeyFileAttribute("keypair.snk")]
 
 namespace TestApplication.StrongNamed;
 
