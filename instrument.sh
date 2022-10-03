3c2ee525feb52158b79bb4dd965a144088f2b6bd
Robert Pająk <pellared@hotmail.com>
2022-10-03 21:54:24 +0200

    Address review comments

--- instrument.sh
+++ instrument.sh
@@ -26,8 +26,6 @@ esac
 test -z "$INSTALL_DIR" && INSTALL_DIR="./otel-dotnet-auto"
 
 
-test -z "$ENABLE_PROFILING" && ENABLE_PROFILING="1"
-
 # check $INSTALL_DIR and use it to set the absolute path $OTEL_DIR 
 if [ -z "$(ls -A $INSTALL_DIR)" ]; then
   echo "There are no files under the location specified via INSTALL_DIR."
@@ -65,25 +63,25 @@ if [ "$ENABLE_PROFILING" = "true" ]; then
   esac
 
   # Enable .NET Framework Profiling API
-  export COR_ENABLE_PROFILING="$ENABLE_PROFILING"
-  export COR_PROFILER="{918728DD-259F-4A6A-AC2B-B85E1B658318}"
-  export COR_PROFILER_PATH="$OTEL_DIR/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
   if [ "$DISTRIBUTION" == "windows" ]
   then
-      # Set paths for both bitness on Windows, see https://docs.microsoft.com/en-us/dotnet/core/run-time-config/debugging-profiling#profiler-location
-      export COR_PROFILER_PATH_64="$OTEL_DIR/win-x64/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
-      export COR_PROFILER_PATH_32="$OTEL_DIR/win-x86/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
+    export COR_ENABLE_PROFILING="$ENABLE_PROFILING"
+    export COR_PROFILER="{918728DD-259F-4A6A-AC2B-B85E1B658318}"
+    # Set paths for both bitness on Windows, see https://docs.microsoft.com/en-us/dotnet/core/run-time-config/debugging-profiling#profiler-location
+    export COR_PROFILER_PATH_64="$OTEL_DIR/win-x64/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
+    export COR_PROFILER_PATH_32="$OTEL_DIR/win-x86/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
   fi
 
   # Enable .NET Core Profiling API
   export CORECLR_ENABLE_PROFILING="${ENABLE_PROFILING}"
   export CORECLR_PROFILER="{918728DD-259F-4A6A-AC2B-B85E1B658318}"
-  export CORECLR_PROFILER_PATH="$OTEL_DIR/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
   if [ "$DISTRIBUTION" == "windows" ]
   then
-      # Set paths for both bitness on Windows, see https://docs.microsoft.com/en-us/dotnet/core/run-time-config/debugging-profiling#profiler-location
-      export CORECLR_PROFILER_PATH_64="$OTEL_DIR/win-x64/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
-      export CORECLR_PROFILER_PATH_32="$OTEL_DIR/win-x86/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
+    # Set paths for both bitness on Windows, see https://docs.microsoft.com/en-us/dotnet/core/run-time-config/debugging-profiling#profiler-location
+    export CORECLR_PROFILER_PATH_64="$OTEL_DIR/win-x64/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
+    export CORECLR_PROFILER_PATH_32="$OTEL_DIR/win-x86/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
+  else
+    export CORECLR_PROFILER_PATH="$OTEL_DIR/OpenTelemetry.AutoInstrumentation.Native.$SUFIX"
   fi
 
   # Configure the bytecode instrumentation configuration file
