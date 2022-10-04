fa11b24c955a9a6977aec38639412b3d2ef68ae0
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-10-04 08:24:11 +0200

    Fix potential null reference exceptions in ActivityHelper  (#1328)

--- src/OpenTelemetry.AutoInstrumentation/Util/ActivityHelper.cs
+++ src/OpenTelemetry.AutoInstrumentation/Util/ActivityHelper.cs
@@ -16,6 +16,7 @@
 
 using System;
 using System.Diagnostics;
+using OpenTelemetry.AutoInstrumentation.Logging;
 using OpenTelemetry.AutoInstrumentation.Tagging;
 using OpenTelemetry.Trace;
 
@@ -23,6 +24,8 @@ namespace OpenTelemetry.AutoInstrumentation.Util;
 
 internal static class ActivityHelper
 {
+    private static readonly ILogger Log = OtelLogging.GetLogger();
+
     /// <summary>
     /// Add the StackTrace and other exception metadata to the span
     /// </summary>
@@ -30,44 +33,42 @@ internal static class ActivityHelper
     /// <param name="exception">The exception.</param>
     public static void SetException(this Activity activity, Exception exception)
     {
-        if (exception != null)
+        if (activity == null)
         {
-            activity.SetStatus(Status.Error.WithDescription(exception.Message));
-            activity.RecordException(exception);
+            Log.Debug("Trying to set exception on null activity.");
+            return;
         }
+
+        if (exception == null)
+        {
+            return;
+        }
+
+        activity.SetStatus(Status.Error.WithDescription(exception.Message));
+        activity.RecordException(exception);
     }
 
     public static Activity StartActivityWithTags(this ActivitySource activitySource, string operationName, ActivityKind kind, ITags tags)
     {
+        if (activitySource == null)
+        {
+            Log.Debug("Trying to start activity on null activity source.");
+            return null;
+        }
+
         var activity = activitySource.StartActivity(operationName, kind);
 
-        // Apply tags
-        if (tags != null)
+        if (activity == null || tags == null)
         {
-            foreach (var entry in tags.GetAllTags())
-            {
-                activity.SetTag(entry.Key, entry.Value);
-            }
+            return activity;
+        }
+
+        // Apply tags
+        foreach (var entry in tags.GetAllTags())
+        {
+            activity.SetTag(entry.Key, entry.Value);
         }
 
         return activity;
-    }
-
-    internal static void DisposeWithException(this Activity activity, Exception exception)
-    {
-        if (activity != null)
-        {
-            try
-            {
-                if (exception != null)
-                {
-                    activity.SetException(exception);
-                }
-            }
-            finally
-            {
-                activity.Dispose();
-            }
-        }
     }
 }
---
+++ test/OpenTelemetry.AutoInstrumentation.Tests/Util/ActivityHelperTests.cs
@@ -0,0 +1,155 @@
+// <copyright file="ActivityHelperTests.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Moq;
+using OpenTelemetry.AutoInstrumentation.Tagging;
+using OpenTelemetry.AutoInstrumentation.Util;
+using Xunit;
+
+namespace OpenTelemetry.AutoInstrumentation.Tests.Util;
+
+public class ActivityHelperTests
+{
+    [Fact]
+    public void SetException_NotThrow_WhenActivityIsNull()
+    {
+        const Activity activity = null;
+
+        var action = () => activity.SetException(new Exception());
+
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public void SetException_NotThrow_WhenExceptionIsNull()
+    {
+        var activity = new Activity("test-operation");
+
+        var action = () =>
+        {
+            activity.SetException(null);
+            activity.Dispose();
+        };
+
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public void SetException_SetsExceptionDetails()
+    {
+        using var activity = new Activity("test-operation");
+
+        var exceptionMessage = "test-message";
+        activity.SetException(new Exception(exceptionMessage));
+
+        using (new AssertionScope())
+        {
+            activity.Tags.First(x => x.Key == "otel.status_code").Value.Should().Be("ERROR");
+            activity.Tags.First(x => x.Key == "otel.status_description").Value.Should().Be(exceptionMessage);
+            activity.Events.Should().HaveCount(1);
+        }
+    }
+
+    [Fact]
+    public void StartActivityWithTags_ReturnsNull_WhenActivitySourceIsNull()
+    {
+        const ActivitySource activitySource = null;
+
+        using var activity = activitySource.StartActivityWithTags("test-operation", ActivityKind.Internal, null);
+
+        activity.Should().BeNull();
+    }
+
+    [Fact]
+    public void StartActivityWithTags_ReturnsNull_WhenActivitySourceDoesNotHaveListener()
+    {
+        using var activitySource = new ActivitySource("test-source");
+
+        using var activity = activitySource.StartActivityWithTags("test-operation", ActivityKind.Internal, null);
+
+        using (new AssertionScope())
+        {
+            activitySource.HasListeners().Should().BeFalse();
+            activity.Should().BeNull();
+        }
+    }
+
+    [Theory]
+    [InlineData(ActivityKind.Internal)]
+    [InlineData(ActivityKind.Server)]
+    [InlineData(ActivityKind.Client)]
+    [InlineData(ActivityKind.Producer)]
+    [InlineData(ActivityKind.Consumer)]
+    public void StartActivityWithTags_ReturnsActivity_WhenThereIsActivityListener(ActivityKind kind)
+    {
+        using var activitySource = new ActivitySource("test-source");
+
+        using var listener = CreateActivityListener(activitySource);
+
+        using var activity = activitySource.StartActivityWithTags("test-operation", kind, null);
+
+        using (new AssertionScope())
+        {
+            activitySource.HasListeners().Should().BeTrue();
+            activity.Should().NotBeNull();
+            activity.Kind.Should().Be(kind);
+        }
+    }
+
+    [Fact]
+    public void StartActivityWithTags_SetsCorrectTags()
+    {
+        var tags = new List<KeyValuePair<string, string>>
+        {
+            new("key1", "value1"),
+            new("key2", "value2")
+        };
+
+        var tagsMock = new Mock<ITags>();
+        tagsMock.Setup(x => x.GetAllTags()).Returns(tags);
+
+        using var activitySource = new ActivitySource("test-source");
+
+        using var listener = CreateActivityListener(activitySource);
+
+        using var activity = activitySource.StartActivityWithTags("test-operation", ActivityKind.Internal, tagsMock.Object);
+
+        tagsMock.Setup(x => x.GetAllTags()).Returns(tags);
+
+        using (new AssertionScope())
+        {
+            activitySource.HasListeners().Should().BeTrue();
+            activity.Should().NotBeNull();
+            activity.Tags.Should().BeEquivalentTo(tags);
+        }
+    }
+
+    private static ActivityListener CreateActivityListener(ActivitySource activitySource)
+    {
+        var listener = new ActivityListener();
+        listener.ShouldListenTo = source => source == activitySource;
+        listener.Sample = (ref ActivityCreationOptions<ActivityContext> _) => ActivitySamplingResult.AllData;
+        ActivitySource.AddActivityListener(listener);
+
+        return listener;
+    }
+}
