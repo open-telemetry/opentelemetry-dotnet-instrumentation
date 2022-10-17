a997328b4575e2d2b6fe7f8d783f3c62124044b8
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-10-17 09:49:07 +0200

    Process metrics (#1427)

---
+++ test/IntegrationTests/ProcessTests.cs
@@ -0,0 +1,50 @@
+// <copyright file="ProcessTests.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Threading.Tasks;
+using IntegrationTests.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IntegrationTests;
+
+public class ProcessTests : TestHelper
+{
+    public ProcessTests(ITestOutputHelper output)
+        : base("Smoke", output)
+    {
+        SetEnvironmentVariable("LONG_RUNNING", "true");
+        SetEnvironmentVariable("OTEL_METRIC_EXPORT_INTERVAL", "100");
+    }
+
+    [Fact]
+    [Trait("Category", "EndToEnd")]
+    public async Task SubmitMetrics()
+    {
+        using var collector = await MockMetricsCollector.Start(Output);
+        collector.Expect("OpenTelemetry.Instrumentation.Process");
+
+        using var process = StartTestApplication(metricsAgentPort: collector.Port, enableClrProfiler: !IsCoreClr());
+        try
+        {
+            collector.AssertExpectations();
+        }
+        finally
+        {
+            process.Kill();
+        }
+    }
+}
