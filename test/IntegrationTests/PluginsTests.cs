15658603079de3dc5399080d769d444b61e39d55
Robert Pająk <pellared@hotmail.com>
2022-10-20 10:57:37 +0200

    PluginsTests

--- test/IntegrationTests/PluginsTests.cs
+++ test/IntegrationTests/PluginsTests.cs
@@ -33,13 +33,13 @@ public class PluginsTests : TestHelper
     [Trait("Category", "EndToEnd")]
     public async Task SubmitsTraces()
     {
+        using var collector = await MockSpansCollector.Start(Output);
+        collector.Expect("MyCompany.MyProduct.MyLibrary");
+
         SetEnvironmentVariable("OTEL_DOTNET_AUTO_PLUGINS", "TestApplication.Plugins.Plugin, TestApplication.Plugins, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+        RunTestApplication(otlpTraceCollectorPort: collector.Port);
 
-        using var collector = await LegacyMockZipkinCollector.Start(Output);
-        RunTestApplication(collector.Port);
-        var spans = await collector.WaitForSpansAsync(1);
-
-        spans.Should().Contain(x => x.Name == "SayHello");
+        collector.AssertExpectations();
     }
 
     [Fact]
