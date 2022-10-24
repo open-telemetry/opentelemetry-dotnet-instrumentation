10f3a795e20e243679da969bd3460745a58afe62
Robert Pająk <pellared@hotmail.com>
2022-10-24 13:38:17 +0200

    Refine HttpTests to use MockSpansCollector

--- test/IntegrationTests/HttpTests.cs
+++ test/IntegrationTests/HttpTests.cs
@@ -15,11 +15,11 @@
 // </copyright>
 
 #if !NETFRAMEWORK
-using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using IntegrationTests.Helpers;
+using OpenTelemetry.Proto.Trace.V1;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -27,12 +27,9 @@ namespace IntegrationTests;
 
 public class HttpTests : TestHelper
 {
-    private const string ServiceName = "TestApplication.Http";
-
     public HttpTests(ITestOutputHelper output)
         : base("Http", output)
     {
-        SetEnvironmentVariable("OTEL_SERVICE_NAME", ServiceName);
     }
 
     [Theory]
@@ -42,49 +39,37 @@ public class HttpTests : TestHelper
     [Trait("Category", "EndToEnd")]
     public async Task SubmitTraces(string propagators)
     {
+        using var collector = await MockSpansCollector.Start(Output);
+        Span clientSpan = null;
+        collector.Expect("OpenTelemetry.Instrumentation.Http", span =>
+        {
+            clientSpan = span;
+            return true;
+        });
+        Span serverSpan = null;
+        collector.Expect("OpenTelemetry.Instrumentation.AspNetCore", span =>
+        {
+            serverSpan = span;
+            return true;
+        });
+        Span manualSpan = null;
+        collector.Expect("TestApplication.Http", span =>
+        {
+            manualSpan = span;
+            return true;
+        });
+
         SetEnvironmentVariable("OTEL_PROPAGATORS", propagators);
         SetEnvironmentVariable("DISABLE_DistributedContextPropagator", "true");
+        RunTestApplication(otlpTraceCollectorPort: collector.Port, enableClrProfiler: !IsCoreClr());
 
-        using var agent = await LegacyMockZipkinCollector.Start(Output);
-
-        const int expectedSpanCount = 3;
-
-        RunTestApplication(agent.Port, enableClrProfiler: !IsCoreClr());
-        var spans = await agent.WaitForSpansAsync(expectedSpanCount);
-
+        collector.AssertExpectations();
         using (new AssertionScope())
         {
-            spans.Count.Should().Be(expectedSpanCount);
-
-            // ASP.NET Core auto-instrumentation is generating spans
-            var httpClientSpan = spans.FirstOrDefault(span => span.Name.Equals("HTTP GET"));
-            var httpServerSpan = spans.FirstOrDefault(span => span.Name.Equals("/test"));
-            var manualSpan = spans.FirstOrDefault(span => span.Name.Equals("manual span"));
-
-            httpClientSpan.Should().NotBeNull();
-            httpServerSpan.Should().NotBeNull();
-            manualSpan.Should().NotBeNull();
-
-            // checking trace hierarchy
-            httpClientSpan.ParentId.HasValue.Should().BeFalse();
-            httpServerSpan.ParentId.Should().Be(httpClientSpan.SpanId);
-            manualSpan.ParentId.Should().Be(httpServerSpan.SpanId);
-
-            httpClientSpan.Service.Should().Be(ServiceName);
-            httpServerSpan.Service.Should().Be(ServiceName);
-            manualSpan.Service.Should().Be(ServiceName);
-
-            var httpClientTags = httpClientSpan.Tags;
-            var httpServerTags = httpServerSpan.Tags;
-
-            httpClientTags.Count.Should().Be(8);
-            httpClientTags["http.method"].Should().Be("GET");
-            httpClientTags["http.host"].Should().Be(httpServerTags["http.host"]);
-            httpClientTags["http.url"].Should().Be(httpServerTags["http.url"]);
-            httpClientTags["http.status_code"].Should().Be("200");
-            httpClientTags["peer.service"].Should().Be(httpServerTags["http.host"]);
-            httpClientTags["span.kind"].Should().Be("client");
-            httpServerTags["span.kind"].Should().Be("server");
+            // testing context propagation via trace hierarchy
+            clientSpan.ParentSpanId.IsEmpty.Should().BeTrue();
+            serverSpan.ParentSpanId.Should().Equal(clientSpan.SpanId);
+            manualSpan.ParentSpanId.Should().Equal(serverSpan.SpanId);
         }
     }
 
