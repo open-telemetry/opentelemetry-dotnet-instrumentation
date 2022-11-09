fc4a9659b18056cc691cdaaf48e9f2cd82b950b7
Robert Pająk <pellared@hotmail.com>
2022-11-09 13:25:00 +0100

    Remove ConfigureAwait(false)
    I think the sync context may be needed for output

--- test/IntegrationTests/Helpers/HealthzHelper.cs
+++ test/IntegrationTests/Helpers/HealthzHelper.cs
@@ -36,7 +36,7 @@ internal static class HealthzHelper
         {
             try
             {
-                var response = await client.GetAsync(healthzUrl).ConfigureAwait(false);
+                var response = await client.GetAsync(healthzUrl);
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
                     return;
@@ -49,7 +49,7 @@ internal static class HealthzHelper
                 output.WriteLine($"Healthz endpoint call failed: {ex.Message}");
             }
 
-            await Task.Delay(intervalMilliseconds).ConfigureAwait(false);
+            await Task.Delay(intervalMilliseconds);
         }
 
         throw new InvalidOperationException($"Healthz endpoint never returned OK: {healthzUrl}");
