af426f54c52eee6a8fad604ecc0d417911c9db6e
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-12-30 11:15:43 +0100

    NServiceBus - support for traces and metrics

---
+++ test/IntegrationTests/NServiceBusTests.cs
@@ -0,0 +1,78 @@
+// <copyright file="NServiceBusTests.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using IntegrationTests.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IntegrationTests;
+
+public class NServiceBusTests : TestHelper
+{
+    public NServiceBusTests(ITestOutputHelper output)
+        : base("NServiceBus", output)
+    {
+    }
+
+    [Fact]
+    [Trait("Category", "EndToEnd")]
+    public void SubmitsTraces()
+    {
+        using var collector = new MockSpansCollector(Output);
+        SetExporter(collector);
+        collector.Expect("NServiceBus.Core");
+
+#if NET462
+        RunTestApplication(new TestSettings
+        {
+            Framework = "net472"
+        });
+#else
+        RunTestApplication();
+#endif
+
+        collector.AssertExpectations();
+    }
+
+    [Fact]
+    [Trait("Category", "EndToEnd")]
+    public void SubmitMetrics()
+    {
+        using var collector = new MockMetricsCollector(Output);
+        SetExporter(collector);
+        collector.Expect("NServiceBus.Core");
+
+        SetEnvironmentVariable("LONG_RUNNING", "true");
+        SetEnvironmentVariable("OTEL_METRIC_EXPORT_INTERVAL", "100");
+
+#if NET462
+        using var process = StartTestApplication(new TestSettings
+        {
+            Framework = "net472"
+        });
+#else
+        using var process = StartTestApplication();
+#endif
+        try
+        {
+            collector.AssertExpectations();
+        }
+        finally
+        {
+            process?.Kill();
+        }
+    }
+}
---
+++ test/test-applications/integrations/TestApplication.NServiceBus/Program.cs
@@ -0,0 +1,54 @@
+// <copyright file="Program.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using NServiceBus;
+using TestApplication.NServiceBus;
+using TestApplication.Shared;
+
+ConsoleHelper.WriteSplashScreen(args);
+
+var endpointConfiguration = new EndpointConfiguration("TestApplication.NServiceBus");
+endpointConfiguration.EnableOpenTelemetry();
+
+var transport = endpointConfiguration.UseTransport<LearningTransport>();
+transport.StorageDirectory(".");
+using var cancellation = new CancellationTokenSource();
+var endpointInstance = await Endpoint.Start(endpointConfiguration, cancellation.Token);
+
+try
+{
+    await endpointInstance.SendLocal(new TestMessage(), cancellation.Token);
+
+    // The "LONG_RUNNING" environment variable is used by tests that access/receive
+    // data that takes time to be produced.
+    var longRunning = Environment.GetEnvironmentVariable("LONG_RUNNING");
+    if (longRunning == "true")
+    {
+        // In this case it is necessary to ensure that the test has a chance to read the
+        // expected data, only by keeping the application alive for some time that can
+        // be ensured. Anyway, tests that set "LONG_RUNNING" env var to true are expected
+        // to kill the process directly.
+        Console.WriteLine("LONG_RUNNING is true, waiting for process to be killed...");
+        Process.GetCurrentProcess().WaitForExit();
+    }
+}
+finally
+{
+    await endpointInstance.Stop(cancellation.Token);
+}
