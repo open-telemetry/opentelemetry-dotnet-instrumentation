6338f2b5d8cb5a8c759c8bd3f001768ea1247505
dszmigielski <dszmigielski@splunk.com>
2023-02-02 15:37:54 +0100

    Revert unintentional doc change

--- src/OpenTelemetry.AutoInstrumentation/Configurations/Propagator.cs
+++ src/OpenTelemetry.AutoInstrumentation/Configurations/Propagator.cs
@@ -22,7 +22,7 @@ namespace OpenTelemetry.AutoInstrumentation.Configurations;
 internal enum Propagator
 {
     /// <summary>
-    /// W3C Information Context propagator.
+    /// W3C Trace Context propagator.
     /// </summary>
     W3CTraceContext,
 
