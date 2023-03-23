0d1a8b115e7fb55ea8a1f4365df5d8612160751a
Mateusz Lach <mateusza@splunk.com>
2023-03-23 10:30:32 +0100

    remove mocks from tests

--- test/OpenTelemetry.AutoInstrumentation.Tests/Diagnostics/SdkSelfDiagnosticsEventListenerTests.cs
+++ test/OpenTelemetry.AutoInstrumentation.Tests/Diagnostics/SdkSelfDiagnosticsEventListenerTests.cs
@@ -17,7 +17,8 @@
 // Source originated from https://github.com/open-telemetry/opentelemetry-dotnet/blob/23609730ddd73c860553de847e67c9b2226cff94/test/OpenTelemetry.Tests/Internal/SelfDiagnosticsEventListenerTest.cs
 
 using System.Diagnostics.Tracing;
-using Moq;
+using FluentAssertions;
+using FluentAssertions.Execution;
 using OpenTelemetry.AutoInstrumentation.Diagnostics;
 using OpenTelemetry.AutoInstrumentation.Logging;
 using Xunit;
@@ -33,35 +34,45 @@ public class SdkSelfDiagnosticsEventListenerTests
     [Fact]
     public void EventSourceSetup_LowerSeverity()
     {
-        var logger = new Mock<IOtelLogger>();
-        logger.Setup(otelLogger => otelLogger.Level).Returns(LogLevel.Error);
+        var testSink = new TestSink();
+        var logger = new InternalLogger(testSink, LogLevel.Error);
+        using var listener = new SdkSelfDiagnosticsEventListener(logger);
 
-        using var listener = new SdkSelfDiagnosticsEventListener(logger.Object);
+        AspNetTelemetryEventSourceForTests.Log.Information();
+        AspNetTelemetryEventSourceForTests.Log.Warning();
+        OpenTelemetrySdkEventSourceForTests.Log.Verbose();
 
-        // Emitting a Verbose event. Or any EventSource event with lower severity than Error.
-        AspNetTelemetryEventSourceForTests.Log.ActivityRestored("123");
-        OpenTelemetrySdkEventSourceForTests.Log.ActivityStarted("Activity started", "1");
+        // Events with both Critical and Error EventLevel are logged as errors.
+        OpenTelemetrySdkEventSourceForTests.Log.Error();
+        OpenTelemetrySdkEventSourceForTests.Log.Critical();
 
-        logger.Verify(otelLogger => otelLogger.Level, Times.Once);
-        logger.VerifyNoOtherCalls();
+        using (new AssertionScope())
+        {
+            testSink.Messages.Count.Should().Be(2);
+            testSink.Messages.Should().OnlyContain(msg => msg.Contains("Error"));
+        }
     }
 
     [Fact]
     public void EventSourceSetup_HigherSeverity()
     {
-        var logger = new Mock<IOtelLogger>();
-        logger.Setup(otelLogger => otelLogger.Level).Returns(LogLevel.Debug);
+        var testSink = new TestSink();
+        var logger = new InternalLogger(testSink, LogLevel.Debug);
+        using var listener = new SdkSelfDiagnosticsEventListener(logger);
 
-        using var listener = new SdkSelfDiagnosticsEventListener(logger.Object);
+        AspNetTelemetryEventSourceForTests.Log.Information();
+        AspNetTelemetryEventSourceForTests.Log.Warning();
+        OpenTelemetrySdkEventSourceForTests.Log.Verbose();
+        OpenTelemetrySdkEventSourceForTests.Log.Error();
 
-        // Emitting a Verbose event. Or any EventSource event with lower severity than Error.
-        AspNetTelemetryEventSourceForTests.Log.ActivityRestored("123");
-        OpenTelemetrySdkEventSourceForTests.Log.ActivityStarted("Activity started", "1");
-
-        logger.Verify(otelLogger => otelLogger.Level, Times.Once);
-        logger.Verify(otelLogger => otelLogger.Information("EventSource={0}, Message={1}", "OpenTelemetry-Instrumentation-AspNet-Telemetry-For-Tests", "Activity restored, Id='123'", false), Times.Once);
-        logger.Verify(otelLogger => otelLogger.Debug("EventSource={0}, Message={1}", "OpenTelemetry-Sdk-For-Tests", "Activity started. OperationName = 'Activity started', Id = '1'.", false), Times.Once);
-        logger.VerifyNoOtherCalls();
+        using (new AssertionScope())
+        {
+            testSink.Messages.Count.Should().Be(4);
+            testSink.Messages.Should().Contain(msg => msg.Contains("Error"));
+            testSink.Messages.Should().Contain(msg => msg.Contains("Warning"));
+            testSink.Messages.Should().Contain(msg => msg.Contains("Information"));
+            testSink.Messages.Should().Contain(msg => msg.Contains("Debug"));
+        }
     }
 
     [EventSource(Name = "OpenTelemetry-Instrumentation-AspNet-Telemetry-For-Tests")]
@@ -73,10 +84,16 @@ public class SdkSelfDiagnosticsEventListenerTests
         {
         }
 
-        [Event(4, Message = "Activity restored, Id='{0}'", Level = EventLevel.Informational)]
-        public void ActivityRestored(string id)
+        [Event(4, Message = "Activity info.", Level = EventLevel.Informational)]
+        public void Information()
         {
-            WriteEvent(4, id);
+            WriteEvent(4);
+        }
+
+        [Event(5, Message = "Activity warning.", Level = EventLevel.Warning)]
+        public void Warning()
+        {
+            WriteEvent(5);
         }
     }
 
@@ -89,10 +106,22 @@ public class SdkSelfDiagnosticsEventListenerTests
         {
         }
 
-        [Event(24, Message = "Activity started. OperationName = '{0}', Id = '{1}'.", Level = EventLevel.Verbose)]
-        public void ActivityStarted(string operationName, string id)
+        [Event(24, Message = "Activity verbose.", Level = EventLevel.Verbose)]
+        public void Verbose()
         {
-            WriteEvent(24, operationName, id);
+            WriteEvent(24);
+        }
+
+        [Event(25, Message = "Activity error.", Level = EventLevel.Error)]
+        public void Error()
+        {
+            WriteEvent(25);
+        }
+
+        [Event(26, Message = "Activity critical.", Level = EventLevel.Critical)]
+        public void Critical()
+        {
+            WriteEvent(26);
         }
     }
 }
