7fd9f73876e5ca6b96d25249516fe692d2109abc
Rajkumar Rangaraj <rajrang@microsoft.com>
2023-04-10 21:30:38 -0700

    PR feedback

--- src/OpenTelemetry.AutoInstrumentation.StartupHook/RulesEngine/OpenTelemetrySdkMinimumVersionRule.cs
+++ src/OpenTelemetry.AutoInstrumentation.StartupHook/RulesEngine/OpenTelemetrySdkMinimumVersionRule.cs
@@ -66,7 +66,7 @@ internal class OpenTelemetrySdkMinimumVersionRule : Rule
             return false;
         }
 
-        logger.Information($"Rule Engine: OpenTelemetrySdkMinimumVersionRule evaluation success.");
+        logger.Information("Rule Engine: OpenTelemetrySdkMinimumVersionRule evaluation success.");
         return true;
     }
 
