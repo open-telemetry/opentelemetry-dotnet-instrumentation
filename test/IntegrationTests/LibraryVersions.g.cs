e11ea9b3c444fbc241f1a2e6ba38f53235c6d4eb
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-04-26 08:23:11 +0200

    Generated files

--- test/IntegrationTests/LibraryVersions.g.cs
+++ test/IntegrationTests/LibraryVersions.g.cs
@@ -64,7 +64,7 @@ public static class LibraryVersion
         new object[] { string.Empty }
 #else
         new object[] { "8.0.0" },
-        new object[] { "8.0.14" },
+        new object[] { "8.0.15" },
 #endif
     };
     public static readonly IReadOnlyCollection<object[]> SqlClient = new List<object[]>
