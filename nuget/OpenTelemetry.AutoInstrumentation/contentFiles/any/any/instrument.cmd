e4901e05d7de085e6f53e9d22d783769d5590b99
Paulo Janotti <pjanotti@splunk.com>
2023-05-25 09:49:22 -0700

    Why auto redirection is disabled on .NET Fx

--- nuget/OpenTelemetry.AutoInstrumentation/contentFiles/any/any/instrument.cmd
+++ nuget/OpenTelemetry.AutoInstrumentation/contentFiles/any/any/instrument.cmd
@@ -8,8 +8,12 @@ set BASE_PATH=%~dp0
 set COR_ENABLE_PROFILING=1
 set COR_PROFILER={918728DD-259F-4A6A-AC2B-B85E1B658318}
 set COR_PROFILER_PATH=%BASE_PATH%OpenTelemetry.AutoInstrumentation.Native.dll
+
 :: On .NET Framework automatic assembly redirection MUST be disabled. This setting
-:: is ignored on .NET.
+:: is ignored on .NET. This is necessary because the NuGet package doesn't bring
+:: the pre-defined versions of the transitive dependencies used in the automatic
+:: redirection. Instead the transitive dependencies versions are determined by
+:: the NuGet version resolution algorithm when building the application.
 set OTEL_DOTNET_AUTO_NETFX_REDIRECT_ENABLED=false
 
 :: Settings for .NET
