64611804b4f3d5470368001647e962c4be8836c3
RassK <rkuusmann@splunk.com>
2023-05-29 15:19:27 +0300

    fix licensing

--- tools/DependencyListGenerator/GlobalSuppressions.cs
+++ tools/DependencyListGenerator/GlobalSuppressions.cs
@@ -18,3 +18,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Reviewed.")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1604:Element documentation should have summary", Justification = "Reviewed.")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "Preserving original license.")]
