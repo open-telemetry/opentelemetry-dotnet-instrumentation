19de75a0771d48ace9a774d62f7437dae5bd8ed0
Paulo Janotti <pjanotti@splunk.com>
2023-06-29 12:45:28 -0700

    Add OpenTracing integration test

--- test/test-applications/integrations/dependency-libs/TestApplication.Shared/ProfilerHelper.cs
+++ test/test-applications/integrations/dependency-libs/TestApplication.Shared/ProfilerHelper.cs
@@ -14,10 +14,7 @@
 // limitations under the License.
 // </copyright>
 
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace TestApplication.Shared;
 
