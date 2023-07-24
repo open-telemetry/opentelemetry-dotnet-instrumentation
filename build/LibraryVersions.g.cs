c2005d863b04a41b8f53b1f17ed1f63bb404cb27
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-07-24 08:02:04 +0200

    Genereated files

--- build/LibraryVersions.g.cs
+++ build/LibraryVersions.g.cs
@@ -95,7 +95,7 @@ public static class LibraryVersion
             new List<string>
             {
                 "6.10.7",
-                "8.0.32.1",
+                "8.1.0",
             }
         },
         {
