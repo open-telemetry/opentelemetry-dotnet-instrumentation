a38edf765db14cbd9e9e88182413c5c8dc9e199b
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-08-02 07:57:38 +0200

    tests - Comment  source of connection string

--- test/test-applications/integrations/TestApplication.Azure/Program.cs
+++ test/test-applications/integrations/TestApplication.Azure/Program.cs
@@ -22,6 +22,7 @@ ConsoleHelper.WriteSplashScreen(args);
 var port = GetBlobServicePortPort(args);
 
 // connection string based on https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azurite?tabs=visual-studio#well-known-storage-account-and-key
+// connection string based on https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azurite?tabs=visual-studio#well-known-storage-account-and-key
 var developerConnectionString = $"DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:{port}/devstoreaccount1;";
 
 // Create a blob service client using the developer connection string
