f2cd526d7f3379638a4fdbec5023acef264fcec9
Paulo Janotti <pjanotti@splunk.com>
2023-08-21 09:50:43 -0700

    Update netfx_assembly_redirection.h

--- src/OpenTelemetry.AutoInstrumentation.Native/netfx_assembly_redirection.h
+++ src/OpenTelemetry.AutoInstrumentation.Native/netfx_assembly_redirection.h
@@ -18,7 +18,7 @@ void CorProfiler::InitNetFxAssemblyRedirectsMap()
     const USHORT auto_major = atoi(AUTO_MAJOR);
 
     assembly_version_redirect_map_.insert({
-        { L"Google.Protobuf", {3, 24, 0, 0} },
+        { L"Google.Protobuf", {3, 24, 1, 0} },
         { L"Grpc.Core", {2, 0, 0, 0} },
         { L"Grpc.Core.Api", {2, 0, 0, 0} },
         { L"Microsoft.Bcl.AsyncInterfaces", {7, 0, 0, 0} },
@@ -52,7 +52,7 @@ void CorProfiler::InitNetFxAssemblyRedirectsMap()
         { L"OpenTelemetry.Instrumentation.Quartz", {1, 0, 0, 3} },
         { L"OpenTelemetry.Instrumentation.Runtime", {1, 5, 0, 0} },
         { L"OpenTelemetry.Instrumentation.SqlClient", {1, 0, 0, 0} },
-        { L"OpenTelemetry.Instrumentation.Wcf", {1, 0, 0, 10} },
+        { L"OpenTelemetry.Instrumentation.Wcf", {1, 0, 0, 11} },
         { L"OpenTelemetry.ResourceDetectors.Azure", {1, 0, 0, 2} },
         { L"OpenTelemetry.ResourceDetectors.Container", {1, 0, 0, 4} },
         { L"OpenTelemetry.Shims.OpenTracing", {1, 0, 0, 0} },
