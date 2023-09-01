ec073ae6ffdd9de64c6d681cb567abc8c11ef1bd
RassK <rkuusmann@splunk.com>
2023-09-02 02:16:13 +0300

    Add NativeLibrary resolver

--- src/OpenTelemetry.AutoInstrumentation.StartupHook/NativeMethods.cs
+++ src/OpenTelemetry.AutoInstrumentation.StartupHook/NativeMethods.cs
@@ -14,12 +14,70 @@
 // limitations under the License.
 // </copyright>
 
+using System.Reflection;
 using System.Runtime.InteropServices;
+using OpenTelemetry.AutoInstrumentation.Helpers;
 
 namespace OpenTelemetry.AutoInstrumentation;
 
 internal static class NativeMethods
 {
-    [DllImport("OpenTelemetry.AutoInstrumentation.Native")]
+    static NativeMethods()
+    {
+        NativeLibrary.SetDllImportResolver(typeof(NativeMethods).Assembly, ImportResolver);
+    }
+
+    [DllImport("OpenTelemetry.AutoInstrumentation.Native", CallingConvention = CallingConvention.Cdecl)]
     public static extern bool IsProfilerAttached();
+
+    private static IntPtr ImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+    {
+        IntPtr libHandle = IntPtr.Zero;
+
+        if (libraryName == "OpenTelemetry.AutoInstrumentation.Native")
+        {
+            var homePath = EnvironmentHelper.GetEnvironmentVariable("OTEL_DOTNET_AUTO_HOME")!;
+            string archDir;
+            string extension;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                archDir = Environment.Is64BitProcess ? "win-x64" : "win-x86";
+                extension = "dll";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                archDir = Environment.Is64BitProcess ? GetLinuxDir(homePath) : throw new PlatformNotSupportedException("32bit Linux is not supported.");
+                extension = "so";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                archDir = Environment.Is64BitProcess ? "osx-x64" : throw new PlatformNotSupportedException("32bit MacOS is not supported.");
+                extension = "dylib";
+            }
+            else
+            {
+                throw new NotSupportedException("Platfrom is not supported");
+            }
+
+            libHandle = NativeLibrary.Load(Path.Combine(homePath, archDir, $"OpenTelemetry.AutoInstrumentation.Native.{extension}"));
+        }
+
+        return libHandle;
+    }
+
+    private static string GetLinuxDir(string homePath)
+    {
+        if (Directory.Exists(Path.Combine(homePath, "linux-x64")))
+        {
+            return "linux-x64";
+        }
+
+        if (Directory.Exists(Path.Combine(homePath, "linux-musl-x64")))
+        {
+            return "linux-musl-x64";
+        }
+
+        throw new PlatformNotSupportedException("Could not determine Linux platform.");
+    }
 }
