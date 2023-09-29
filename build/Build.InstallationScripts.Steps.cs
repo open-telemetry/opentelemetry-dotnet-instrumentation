9ccd2174be3c5e14f0b159a7e0e7543516318d28
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-09-29 20:14:19 +0200

    Automate script versioning (#2984)

---
+++ build/Build.InstallationScripts.Steps.cs
@@ -0,0 +1,23 @@
+using Nuke.Common;
+using Nuke.Common.IO;
+
+partial class Build
+{
+    AbsolutePath InstallationScriptsDirectory => OutputDirectory / "installation-scripts";
+
+    Target BuildInstallationScripts => _ => _
+        .After(Clean)
+        .After(CreateRequiredDirectories)
+        .Executes(() =>
+        {
+            var scriptTemplates = RootDirectory / "script-templates";
+            var templateFiles = scriptTemplates.GetFiles();
+            foreach (var templateFile in templateFiles)
+            {
+                var scriptFile = InstallationScriptsDirectory / templateFile.Name.Replace(".template", "");
+                FileSystemTasks.CopyFile(templateFile, scriptFile);
+                scriptFile.UpdateText(x =>
+                    x.Replace("{{VERSION}}", VersionHelper.GetVersion()));
+            }
+        });
+}
--- docs/troubleshooting.md
+++ docs/troubleshooting.md
@@ -186,9 +186,9 @@ of assemblies loaded as domain-neutral.
 different versions of some assemblies also shipped in the `netfx` folder.
 
 If you are having problems in one of the scenarios above run again the
-`Install-OpenTelemetryCore` command from the
-[PowerShell installation module](../OpenTelemetry.DotNet.Auto.psm1)
-to ensure that the required GAC installations are updated.
+`Install-OpenTelemetryCore` command from the PowerShell installation module
+`OpenTelemetry.DotNet.Auto.psm1` to ensure that the required GAC installations
+are updated.
 
 For more information about the GAC usage by the automatic instrumentation,
 see [here](https://github.com/open-telemetry/opentelemetry-dotnet-instrumentation/issues/1906#issuecomment-1376292814).
