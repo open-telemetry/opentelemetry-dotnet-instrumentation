f528de7b9da450b46677ebc8cb41c011ea86548c
Mateusz Lach <mateusza@splunk.com>
2023-11-07 14:23:43 +0100

    add links to semantic conventions repo

--- src/OpenTelemetry.AutoInstrumentation/Instrumentations/Kafka/MessagingAttributes.cs
+++ src/OpenTelemetry.AutoInstrumentation/Instrumentations/Kafka/MessagingAttributes.cs
@@ -16,6 +16,7 @@
 
 namespace OpenTelemetry.AutoInstrumentation.Instrumentations.Kafka;
 
+// https://github.com/open-telemetry/semantic-conventions/blob/v1.22.0/docs/messaging/messaging-spans.md#messaging-attributes
 internal static class MessagingAttributes
 {
     internal static class Keys
@@ -25,13 +26,14 @@ internal static class MessagingAttributes
         public const string DestinationName = "messaging.destination.name";
         public const string ClientId = "messaging.client_id";
 
+        // https://github.com/open-telemetry/semantic-conventions/blob/v1.22.0/docs/messaging/kafka.md#span-attributes
         internal static class Kafka
         {
+            public const string ConsumerGroupId = "messaging.kafka.consumer.group";
             public const string Partition = "messaging.kafka.destination.partition";
             public const string MessageKey = "messaging.kafka.message.key";
             public const string PartitionOffset = "messaging.kafka.message.offset";
             public const string IsTombstone = "messaging.kafka.message.tombstone";
-            public const string ConsumerGroupId = "messaging.kafka.consumer.group";
         }
     }
 
