31f2037fdf887d19e37acd58ac6b5a84ee831f44
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-11-15 08:44:18 +0100

    Genereated files

--- test/IntegrationTests/LibraryVersions.g.cs
+++ test/IntegrationTests/LibraryVersions.g.cs
@@ -37,7 +37,7 @@ public static class LibraryVersion
         new object[] { string.Empty }
 #else
         new object[] { "6.0.12" },
-        new object[] { "7.0.13" },
+        new object[] { "8.0.0" },
 #endif
     };
     public static readonly IReadOnlyCollection<object[]> EntityFrameworkCorePomeloMySql = new List<object[]>
