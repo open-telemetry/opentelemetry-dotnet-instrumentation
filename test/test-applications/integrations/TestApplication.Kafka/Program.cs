74019224f1921423e1ebd1c4132bff777bfc495e
Mateusz Lach <mateusza@splunk.com>
2023-11-30 15:19:30 +0100

    increase cancellation delay

--- test/test-applications/integrations/TestApplication.Kafka/Program.cs
+++ test/test-applications/integrations/TestApplication.Kafka/Program.cs
@@ -156,6 +156,7 @@ internal static class Program
             BootstrapServers = bootstrapServers,
             // https://github.com/confluentinc/confluent-kafka-dotnet/tree/07de95ed647af80a0db39ce6a8891a630423b952#basic-consumer-example
             AutoOffsetReset = AutoOffsetReset.Earliest,
+            CancellationDelayMaxMs = 5000,
             EnableAutoCommit = true
         };
 
