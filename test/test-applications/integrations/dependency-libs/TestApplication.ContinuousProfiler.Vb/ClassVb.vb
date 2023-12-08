76fa7b76b1584c67e8c792ba1882a50744e96269
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-12-08 06:50:10 +0100

    Simplify copyright disclaimer

--- test/test-applications/integrations/dependency-libs/TestApplication.ContinuousProfiler.Vb/ClassVb.vb
+++ test/test-applications/integrations/dependency-libs/TestApplication.ContinuousProfiler.Vb/ClassVb.vb
@@ -1,18 +1,5 @@
-' <copyright file="ClassA.cs" company="OpenTelemetry Authors">
 ' Copyright The OpenTelemetry Authors
-'
-' Licensed under the Apache License, Version 2.0 (the "License");
-' you may Not use this file except in compliance with the License.
-' You may obtain a copy of the License at
-'
-'     http://www.apache.org/licenses/LICENSE-2.0
-'
-' Unless required by applicable law Or agreed to in writing, software
-' distributed under the License Is distributed on an "AS IS" BASIS,
-' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
-' See the License for the specific language governing permissions And
-' limitations under the License.
-' </copyright>
+' SPDX-License-Identifier: Apache-2.0
 
 Public Class ClassVb
     Public Shared Sub MethodVb(testParam As String)
