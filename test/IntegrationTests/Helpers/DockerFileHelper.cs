ded807bc85c83ee4040f676fd62a0fe0117e7618
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-03-14 09:41:43 +0100

    Typo fix

--- test/IntegrationTests/Helpers/DockerFileHelper.cs
+++ test/IntegrationTests/Helpers/DockerFileHelper.cs
@@ -7,9 +7,9 @@ internal static class DockerFileHelper
 {
     public static string ReadImageFrom(string dockerfile)
     {
-        string raw = File.ReadAllText($"./docker/{dockerfile}");
+        var raw = File.ReadAllText($"./docker/{dockerfile}");
 
-        // Exlcude FROM
+        // Exclude FROM
         return raw.Substring(4).Trim();
     }
 }
