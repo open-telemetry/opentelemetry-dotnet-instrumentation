0a3cb6e8b05ae8277ce99a457959f7d95b79b313
Rajkumar Rangaraj <rajrang@microsoft.com>
2024-03-15 20:27:39 -0700

    [Dotnet Monitor Experimental] Add getting started for metrics usage using dotnet-monitor (#3318)
    * getting-started-dotnet-monitor-metrics
    
    * fix markdownlint
    
    * Raname NextGen.AutoInstrumentation to just next-gen
    
    * Remove logger code.

---
+++ next-gen/docs/getting-started-dotnet-monitor-metrics/Program.cs
@@ -0,0 +1,27 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Diagnostics.Metrics;
+
+public class Program
+{
+    private static readonly Meter MyMeter = new("MyCompany.MyProduct.MyLibrary", "1.0");
+    private static readonly Counter<long> MyFruitCounter = MyMeter.CreateCounter<long>("MyFruitCounter");
+
+    public static void Main()
+    {
+        Console.WriteLine("Press any key to exit");
+
+        while (!Console.KeyAvailable)
+        {
+            MyFruitCounter.Add(1, new("name", "apple"), new("color", "red"));
+            MyFruitCounter.Add(2, new("name", "lemon"), new("color", "yellow"));
+            MyFruitCounter.Add(1, new("name", "lemon"), new("color", "yellow"));
+            MyFruitCounter.Add(2, new("name", "apple"), new("color", "green"));
+            MyFruitCounter.Add(5, new("name", "apple"), new("color", "red"));
+            MyFruitCounter.Add(4, new("name", "lemon"), new("color", "yellow"));
+
+            Thread.Sleep(300);
+        }
+    }
+}
