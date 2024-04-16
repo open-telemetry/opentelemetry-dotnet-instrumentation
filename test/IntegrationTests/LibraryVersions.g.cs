0dcb5a8b3b4904d124425f72359bf54b3dfe8fe5
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-04-16 06:49:36 +0200

    Tests - Bump Elastic.Clients.Elasticsearch to 8.13.5

--- test/IntegrationTests/LibraryVersions.g.cs
+++ test/IntegrationTests/LibraryVersions.g.cs
@@ -29,7 +29,7 @@ public static partial class LibraryVersion
 #else
         new object[] { "8.0.0" },
         new object[] { "8.10.0" },
-        new object[] { "8.13.3" },
+        new object[] { "8.13.5" },
 #endif
     };
     public static readonly IReadOnlyCollection<object[]> EntityFrameworkCore = new List<object[]>
