ab8b2a31bedb743d9417ee2d7de55b015b564d0d
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-06-18 10:08:31 +0200

    [Tests] Bump Elastic.Clients.Elasticsearch to 8.14.2

--- test/IntegrationTests/LibraryVersions.g.cs
+++ test/IntegrationTests/LibraryVersions.g.cs
@@ -37,7 +37,7 @@ public static partial class LibraryVersion
 #else
             theoryData.Add("8.0.0");
             theoryData.Add("8.10.0");
-            theoryData.Add("8.14.0");
+            theoryData.Add("8.14.2");
 #endif
             return theoryData;
         }
