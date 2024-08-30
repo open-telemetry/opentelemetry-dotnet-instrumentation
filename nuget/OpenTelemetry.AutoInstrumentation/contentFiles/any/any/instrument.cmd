3994e93f65f1b0991fe0f953bbf5276823e88920
Mateusz Łach <mateusza@splunk.com>
2024-08-30 20:53:31 +0200

    [nuget] update launch scripts to verify native profiler path (#3594)
    * verify existence of native profiler at expected locations before attempting to run the app with launch scripts

--- nuget/OpenTelemetry.AutoInstrumentation/contentFiles/any/any/instrument.cmd
+++ nuget/OpenTelemetry.AutoInstrumentation/contentFiles/any/any/instrument.cmd
@@ -1,13 +1,42 @@
 @echo off
-setlocal
+setlocal ENABLEDELAYEDEXPANSION
 
 :: This script is expected to be used in a build that specified a RuntimeIdentifier (RID)
 set BASE_PATH=%~dp0
+set COR_PROFILER_PATH=%BASE_PATH%OpenTelemetry.AutoInstrumentation.Native.dll
+
+:: Validate
+IF EXIST %COR_PROFILER_PATH% (
+    set CORECLR_PROFILER_PATH=!COR_PROFILER_PATH!
+) ELSE (
+    set "COR_PROFILER_PATH="
+    echo RuntimeIdentifier was not specified when building the project. Attempting to use the fallback location.
+
+    set COR_PROFILER_PATH_64=%BASE_PATH%runtimes\win-x64\native\OpenTelemetry.AutoInstrumentation.Native.dll
+    set COR_PROFILER_PATH_32=%BASE_PATH%runtimes\win-x86\native\OpenTelemetry.AutoInstrumentation.Native.dll
+
+    IF EXIST !COR_PROFILER_PATH_64! (
+        IF EXIST !COR_PROFILER_PATH_32! (
+            set CORECLR_PROFILER_PATH_32=!COR_PROFILER_PATH_32!
+        ) ELSE (
+            set "COR_PROFILER_PATH_32="
+        )
+        set CORECLR_PROFILER_PATH_64=!COR_PROFILER_PATH_64!
+    ) ELSE (
+        set "COR_PROFILER_PATH_64="
+        IF EXIST !COR_PROFILER_PATH_32! (
+            set CORECLR_PROFILER_PATH_32=!COR_PROFILER_PATH_32!
+        ) ELSE (
+            set "COR_PROFILER_PATH_32="
+            echo Unable to locate the native profiler. 1>&2
+            exit /b 1
+        )
+    )
+)
 
 :: Settings for .NET Framework
 set COR_ENABLE_PROFILING=1
 set COR_PROFILER={918728DD-259F-4A6A-AC2B-B85E1B658318}
-set COR_PROFILER_PATH=%BASE_PATH%OpenTelemetry.AutoInstrumentation.Native.dll
 
 :: On .NET Framework automatic assembly redirection MUST be disabled. This setting
 :: is ignored on .NET. This is necessary because the NuGet package doesn't bring
@@ -20,7 +49,6 @@ set OTEL_DOTNET_AUTO_NETFX_REDIRECT_ENABLED=false
 set ASPNETCORE_HOSTINGSTARTUPASSEMBLIES=OpenTelemetry.AutoInstrumentation.AspNetCoreBootstrapper
 set CORECLR_ENABLE_PROFILING=1
 set CORECLR_PROFILER={918728DD-259F-4A6A-AC2B-B85E1B658318}
-set CORECLR_PROFILER_PATH=%BASE_PATH%OpenTelemetry.AutoInstrumentation.Native.dll
 set DOTNET_STARTUP_HOOKS=%BASE_PATH%OpenTelemetry.AutoInstrumentation.StartupHook.dll
 
 :: Settings for OpenTelemetry
