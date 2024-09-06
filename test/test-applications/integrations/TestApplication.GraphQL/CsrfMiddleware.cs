372940ad681889993f36e56a8ba534499fb765bf
Yevhenii Solomchenko <ysolomchenko@splunk.com>
2024-09-06 14:10:44 +0200

    make documentation for CsrfMiddleware

--- test/test-applications/integrations/TestApplication.GraphQL/CsrfMiddleware.cs
+++ test/test-applications/integrations/TestApplication.GraphQL/CsrfMiddleware.cs
@@ -3,6 +3,12 @@
 
 namespace TestApplication.GraphQL;
 
+/// <summary>
+/// Middleware to handle CSRF protection for GraphQL GET requests.
+/// GraphQL servers require the "GraphQL-Require-Preflight" header to bypass
+/// CSRF checks for GET requests. This middleware adds the header to ensure
+/// that GET requests are not blocked due to missing CSRF tokens.
+/// </summary>
 public class CsrfMiddleware
 {
     private readonly RequestDelegate _next;
