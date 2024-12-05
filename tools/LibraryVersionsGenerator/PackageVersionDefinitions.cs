7baac594b075e81bfba3a42e3dbfc334227aabf0
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-12-05 11:15:48 +0100

    document vulnerabilities in generator

--- tools/LibraryVersionsGenerator/PackageVersionDefinitions.cs
+++ tools/LibraryVersionsGenerator/PackageVersionDefinitions.cs
@@ -133,6 +133,7 @@ internal static class PackageVersionDefinitions
             TestApplicationName = "TestApplication.MongoDB",
             Versions = new List<PackageVersion>
             {
+                // new("2.7.0"), - high vulnerability https://github.com/advisories/GHSA-7j9m-j397-g4wx, < 2.19.0
                 new("2.19.0"),
                 new("2.30.0"),
                 new("3.0.0", supportedTargetFrameworks: ["net9.0", "net8.0", "net472"], supportedExecutionFrameworks: ["net9.0", "net8.0", "net462"]),
