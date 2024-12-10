7cb652507d387d2869a18b18bd48435d466a484c
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-12-10 11:47:03 +0100

    temp

--- test/test-applications/integrations/TestApplication.Modules/Program.cs
+++ test/test-applications/integrations/TestApplication.Modules/Program.cs
@@ -4,29 +4,39 @@
 using Newtonsoft.Json;
 using TestApplication.Shared;
 
-namespace TestApplication.Smoke;
+namespace TestApplication.Modules;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        ConsoleHelper.WriteSplashScreen(args);
-
-        if (args.Length < 2)
+        try
         {
-            throw new ArgumentException("Temp path is not provided. Use '--temp-path /my/path/to/temp_file'");
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionEventHandler;
+            ConsoleHelper.WriteSplashScreen(args);
+            if (args.Length < 2)
+            {
+                throw new ArgumentException("Temp path is not provided. Use '--temp-path /my/path/to/temp_file'");
+            }
+
+            var contents = JsonConvert.SerializeObject((from x in AppDomain.CurrentDomain.GetAssemblies()
+                                                           select x.GetName().Name into name
+                                                           where name?.StartsWith("OpenTelemetry") ?? false
+                                                           orderby name
+                                                           select name).ToList());
+            File.WriteAllText(args[1], contents);
         }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception from Main " + e);
+            throw;
+        }
+    }
 
-        var otelLibs = AppDomain.CurrentDomain
-            .GetAssemblies()
-            .Select(x => x.GetName().Name)
-            .Where(name => name != null && name.StartsWith("OpenTelemetry"))
-            .OrderBy(name => name)
-            .ToList();
-
-        var json = JsonConvert.SerializeObject(otelLibs);
-        var path = args[1];
-
-        File.WriteAllText(path, json);
+    private static void UnhandledExceptionEventHandler(object sender, UnhandledExceptionEventArgs args)
+    {
+        var ex = (Exception)args.ExceptionObject;
+        Console.WriteLine("UnhandledExceptionEventHandler caught : " + ex.Message);
+        Console.WriteLine("Runtime terminating: {0}", args.IsTerminating);
     }
 }
