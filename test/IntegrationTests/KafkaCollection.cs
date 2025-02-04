d46428519b65656abde05d6920871b4c7f5eb3fc
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-02-04 13:04:55 +0100

    [Tests] Kafka - use custom port (#4006)

--- test/IntegrationTests/KafkaCollection.cs
+++ test/IntegrationTests/KafkaCollection.cs
@@ -4,6 +4,7 @@
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
 using DotNet.Testcontainers.Networks;
+using IntegrationTests.Helpers;
 using static IntegrationTests.Helpers.DockerFileHelper;
 
 namespace IntegrationTests;
@@ -19,21 +20,30 @@ public class KafkaCollection : ICollectionFixture<KafkaFixture>
 /// </summary>
 public class KafkaFixture : IAsyncLifetime
 {
-    private const int KafkaPort = 9092;
     private const int ZookeeperClientPort = 2181;
-    private const string KafkaContainerName = "integration-test-kafka";
-    private const string TestNetworkName = $"{KafkaContainerName}-network";
-    private const string ZookeeperContainerName = $"{KafkaContainerName}-zookeeper";
     private static readonly string ZooKeeperImage = ReadImageFrom("zookeeper.Dockerfile");
     private static readonly string KafkaImage = ReadImageFrom("kafka.Dockerfile");
+    private readonly string _kafkaContainerName;
+    private readonly string _testNetworkName;
+    private readonly string _zookeeperContainerName;
     private IContainer? _kafkaContainer;
     private IContainer? _zooKeeperContainer;
     private INetwork? _containerNetwork;
 
+    public KafkaFixture()
+    {
+        Port = TcpPortProvider.GetOpenPort();
+        _kafkaContainerName = "integration-test-kafka" + Port;
+        _testNetworkName = $"{_kafkaContainerName}-network";
+        _zookeeperContainerName = $"{_kafkaContainerName}-zookeeper";
+    }
+
+    public int Port { get; }
+
     public async Task InitializeAsync()
     {
         _containerNetwork = new NetworkBuilder()
-            .WithName(TestNetworkName)
+            .WithName(_testNetworkName)
             .Build();
         await _containerNetwork.CreateAsync();
         _zooKeeperContainer = await LaunchZookeeper(_containerNetwork);
@@ -58,11 +68,11 @@ public class KafkaFixture : IAsyncLifetime
         }
     }
 
-    private static async Task<IContainer?> LaunchZookeeper(INetwork? containerNetwork)
+    private async Task<IContainer?> LaunchZookeeper(INetwork? containerNetwork)
     {
         var container = new ContainerBuilder()
             .WithImage(ZooKeeperImage)
-            .WithName(ZookeeperContainerName)
+            .WithName(_zookeeperContainerName)
             .WithEnvironment("ZOOKEEPER_CLIENT_PORT", ZookeeperClientPort.ToString())
             .WithEnvironment("ZOOKEEPER_TICK_TIME", "2000")
             .WithNetwork(containerNetwork)
@@ -72,7 +82,7 @@ public class KafkaFixture : IAsyncLifetime
         return container;
     }
 
-    private static async Task<IContainer?> LaunchKafkaContainer(
+    private async Task<IContainer?> LaunchKafkaContainer(
         INetwork? containerNetwork,
         IContainer? zooKeeperContainer)
     {
@@ -80,12 +90,12 @@ public class KafkaFixture : IAsyncLifetime
         var zookeeperContainerName = zooKeeperContainer?.Name.Substring(1);
         var container = new ContainerBuilder()
             .WithImage(KafkaImage)
-            .WithName(KafkaContainerName)
-            .WithPortBinding(KafkaPort)
+            .WithName(_kafkaContainerName)
+            .WithPortBinding(Port)
             .WithEnvironment("KAFKA_BROKER_ID", "1")
             .WithEnvironment("KAFKA_AUTO_CREATE_TOPICS_ENABLE", "false")
             .WithEnvironment("KAFKA_ZOOKEEPER_CONNECT", $"{zookeeperContainerName}:{ZookeeperClientPort}")
-            .WithEnvironment("KAFKA_ADVERTISED_LISTENERS", $"PLAINTEXT://{KafkaContainerName}:29092,PLAINTEXT_HOST://localhost:{KafkaPort}")
+            .WithEnvironment("KAFKA_ADVERTISED_LISTENERS", $"PLAINTEXT://{_kafkaContainerName}:29092,PLAINTEXT_HOST://localhost:{Port}")
             .WithEnvironment("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT")
             .WithEnvironment("KAFKA_INTER_BROKER_LISTENER_NAME", "PLAINTEXT")
             .WithEnvironment("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
