173b0109a647a4dca084ef4ab94b768eddae3a1d
Nand Vinchhi <nand@getairchat.com>
2025-03-04 15:28:59 -0800

    otel stuff

--- src/OpenTelemetry.AutoInstrumentation/Instrumentations/Axal/AxalIntegration.cs
+++ src/OpenTelemetry.AutoInstrumentation/Instrumentations/Axal/AxalIntegration.cs
@@ -2,7 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using OpenTelemetry.AutoInstrumentation.CallTarget;
+using OpenTelemetry.AutoInstrumentation.Util;
 
 namespace OpenTelemetry.AutoInstrumentation.Instrumentations.Axal;
 
@@ -10,17 +13,19 @@ namespace OpenTelemetry.AutoInstrumentation.Instrumentations.Axal;
 /// BusinessLogic instrumentation for ProcessBusinessOperation method
 /// </summary>
 [InstrumentMethod(
-assemblyName: "?Examples",
-typeName: "?Examples.AspNetCoreMvc.Logic",
-methodName: "?",
-returnTypeName: ClrNames.Void,
-parameterTypeNames: new string[] { },
-minimumVersion: "1.0.0",
-maximumVersion: "65535.65535.65535",
-integrationName: "Axal",
-type: InstrumentationType.Trace)]
+    assemblyName: "?Examples",
+    typeName: "?Examples.AspNetCoreMvc.Logic",
+    methodName: "?",
+    returnTypeName: ClrNames.Void,
+    parameterTypeNames: new string[] { },
+    minimumVersion: "1.0.0",
+    maximumVersion: "65535.65535.65535",
+    integrationName: "Axal",
+    type: InstrumentationType.Trace)]
 public static class AxalIntegration
 {
+    private static readonly ActivitySource Source = new("OpenTelemetry.AutoInstrumentation.Axal");
+
     /// <summary>
     /// OnMethodBegin callback
     /// </summary>
@@ -31,21 +36,48 @@ public static class AxalIntegration
     /// <returns>Calltarget state value</returns>
     internal static CallTargetState OnMethodBegin<TTarget, TArg1>(TTarget instance, TArg1 arg1)
     {
-        Console.WriteLine("HELLO WORLD");
-        return new CallTargetState(null, null);
+        var typeName = typeof(TTarget).FullName ?? "Unknown";
+        var activity = Source.StartActivity($"Axal.{typeName}", ActivityKind.Internal);
+        if (activity is { IsAllDataRequested: true })
+        {
+            var argTypes = new List<string> { typeof(TArg1).FullName ?? "Unknown" };
+            activity.SetTag("axal.class.name", typeName);
+            activity.SetTag("axal.argument.type.names", string.Join(",", argTypes));
+        }
+
+        // Return the state with the activity so it can be accessed in OnMethodEnd
+        return new CallTargetState(activity, null);
     }
 
     /// <summary>
     /// OnMethodEnd callback
     /// </summary>
     /// <typeparam name="TTarget">Type of the target</typeparam>
+    /// <typeparam name="TReturn">Return type of the method</typeparam>
     /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
     /// <param name="returnValue">Return value</param>
     /// <param name="exception">Exception instance in case the original code threw an exception.</param>
     /// <param name="state">Calltarget state value</param>
     /// <returns>A response value, in an async scenario will be T of Task of T</returns>
-    internal static CallTargetReturn<string> OnMethodEnd<TTarget>(TTarget instance, string returnValue, Exception exception, in CallTargetState state)
+    internal static CallTargetReturn<TReturn> OnMethodEnd<TTarget, TReturn>(TTarget instance, TReturn returnValue, Exception exception, in CallTargetState state)
     {
-        return new CallTargetReturn<string>(returnValue);
+        var activity = state.Activity;
+        if (activity != null)
+        {
+            activity.SetTag("return.type.name", typeof(TReturn).FullName ?? "Unknown");
+            if (exception != null)
+            {
+                activity.SetException(exception);
+                activity.SetStatus(ActivityStatusCode.Error, exception.Message);
+            }
+            else
+            {
+                activity.SetStatus(ActivityStatusCode.Ok);
+            }
+
+            activity.Stop();
+        }
+
+        return new CallTargetReturn<TReturn>(returnValue);
     }
 }
