5a76922e904e05fe8d7ea91c2f157cfc1f18fe5b
Igor Kiselev <ikisel@cisco.com>
2025-05-07 08:38:21 -0700

    Added comment about callers of AppConfigUpdater.ModifyConfig

--- src/OpenTelemetry.AutoInstrumentation.Loader/AppConfigUpdater.cs
+++ src/OpenTelemetry.AutoInstrumentation.Loader/AppConfigUpdater.cs
@@ -17,7 +17,10 @@ namespace OpenTelemetry.AutoInstrumentation.Loader;
 public static class AppConfigUpdater
 {
     /// <summary>
-    /// Modify assembly bindings in appDomainSetup
+    /// Modify assembly bindings in appDomainSetup.
+    /// Will be called through reflection when new <see cref="System.AppDomain"/> created.
+    /// Call done using bytecode modifications for <see cref="AppDomain.CreateDomain(string,System.Security.Policy.Evidence,AppDomainSetup)"/>
+    /// and <see cref="AppDomainManager.CreateDomainHelper(string,System.Security.Policy.Evidence,System.AppDomainSetup)"/>.
     /// </summary>
     /// <param name="appDomainSetup">appDomainSetup to be updated</param>
     public static void ModifyConfig(AppDomainSetup appDomainSetup)
