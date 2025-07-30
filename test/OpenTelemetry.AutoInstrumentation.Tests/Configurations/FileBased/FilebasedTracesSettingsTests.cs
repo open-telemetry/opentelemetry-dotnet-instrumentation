03ca73c15b37caf0381986bbf9f339a443060085
Yevhenii Solomchenko <ysolomchenko@splunk.com>
2025-07-30 16:25:18 +0200

    remove unused usings + minor fix

--- test/OpenTelemetry.AutoInstrumentation.Tests/Configurations/FileBased/FilebasedTracesSettingsTests.cs
+++ test/OpenTelemetry.AutoInstrumentation.Tests/Configurations/FileBased/FilebasedTracesSettingsTests.cs
@@ -3,7 +3,6 @@
 
 using OpenTelemetry.AutoInstrumentation.Configurations;
 using OpenTelemetry.AutoInstrumentation.Configurations.FileBasedConfiguration;
-using OpenTelemetry.Trace;
 using Xunit;
 
 namespace OpenTelemetry.AutoInstrumentation.Tests.Configurations.FileBased;
