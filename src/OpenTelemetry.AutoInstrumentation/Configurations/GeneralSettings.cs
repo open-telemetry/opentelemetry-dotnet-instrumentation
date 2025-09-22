74f5b2d66d84947873248cb22b3958bbf9fc5b86
Yevhenii Solomchenko <ysolomchenko@splunk.com>
2025-09-22 09:43:57 +0200

    [File-Based] Add General Settings

--- src/OpenTelemetry.AutoInstrumentation/Configurations/GeneralSettings.cs
+++ src/OpenTelemetry.AutoInstrumentation/Configurations/GeneralSettings.cs
@@ -1,6 +1,8 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+using OpenTelemetry.AutoInstrumentation.Configurations.FileBasedConfiguration;
+
 namespace OpenTelemetry.AutoInstrumentation.Configurations;
 
 internal class GeneralSettings : Settings
@@ -8,7 +10,7 @@ internal class GeneralSettings : Settings
     /// <summary>
     /// Gets the list of plugins represented by <see cref="Type.AssemblyQualifiedName"/>.
     /// </summary>
-    public IList<string> Plugins { get; } = new List<string>();
+    public IList<string> Plugins { get; } = [];
 
     /// <summary>
     /// Gets a value indicating whether the <see cref="AppDomain.UnhandledException"/> event should trigger
@@ -42,5 +44,11 @@ internal class GeneralSettings : Settings
         SetupSdk = configuration.GetBool(ConfigurationKeys.SetupSdk) ?? true;
 
         ProfilerEnabled = configuration.GetString(ConfigurationKeys.ProfilingEnabled) == "1";
+    }
+
+    protected override void OnLoadFile(YamlConfiguration configuration)
+    {
+        SetupSdk = !configuration.Disabled;
+        FlushOnUnhandledException = configuration.FlushOnUnhandledException;
     }
 }
