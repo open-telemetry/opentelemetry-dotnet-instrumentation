e1ff20f85018107dd5db7be4ce3f36773b3d3bde
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-09-29 13:55:07 +0200

    missing file

--- src/OpenTelemetry.AutoInstrumentation/Configurations/FileBasedConfiguration/NoCodeSignature.cs
+++ src/OpenTelemetry.AutoInstrumentation/Configurations/FileBasedConfiguration/NoCodeSignature.cs
@@ -8,7 +8,7 @@ namespace OpenTelemetry.AutoInstrumentation.Configurations.FileBasedConfiguratio
 internal class NoCodeSignature
 {
     [YamlMember(Alias = "return_type")]
-    public string? ReturnedType { get; set; }
+    public string? ReturnType { get; set; }
 
     [YamlMember(Alias = "parameter_types")]
     public string[]? ParameterTypes { get; set; }
