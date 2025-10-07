dd59b24e1596198b9e0ccd5b861794ffb4ea3d53
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-10-07 06:07:20 +0200

    Configuration based instrumentation - span kind (#4503)

--- src/OpenTelemetry.AutoInstrumentation/Configurations/NoCodeSettings.cs
+++ src/OpenTelemetry.AutoInstrumentation/Configurations/NoCodeSettings.cs
@@ -1,6 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Diagnostics;
 using OpenTelemetry.AutoInstrumentation.Configurations.FileBasedConfiguration;
 using OpenTelemetry.AutoInstrumentation.Logging;
 using static OpenTelemetry.AutoInstrumentation.InstrumentationDefinitions;
@@ -143,9 +144,11 @@ internal class NoCodeSettings : Settings
                 AssemblyFullName,
                 $"OpenTelemetry.AutoInstrumentation.Instrumentations.NoCode.NoCodeIntegration{parametersCount}");
 
+            var activityKind = ParseActivityKind(noCodeEntry.Span.Kind);
+
             Log.Debug($"NoCode adding instrumentation for assembly: '{noCodeTarget.Assembly.Name}', type: '{noCodeTarget.Type}', method: '{noCodeTarget.Method}' with signature: '{string.Join(",", targetSignatureTypes)}'");
 
-            instrumentedMethods.Add(new NoCodeInstrumentedMethod(definition, targetSignatureTypes, noCodeEntry.Span.Name));
+            instrumentedMethods.Add(new NoCodeInstrumentedMethod(definition, targetSignatureTypes, noCodeEntry.Span.Name, activityKind));
         }
 
         if (instrumentedMethods.Count > 0)
@@ -153,5 +156,18 @@ internal class NoCodeSettings : Settings
             Enabled = true;
             InstrumentedMethods = instrumentedMethods;
         }
+    }
+
+    private static ActivityKind ParseActivityKind(string? kindString)
+    {
+        return kindString switch
+        {
+            "internal" => ActivityKind.Internal,
+            "server" => ActivityKind.Server,
+            "client" => ActivityKind.Client,
+            "producer" => ActivityKind.Producer,
+            "consumer" => ActivityKind.Consumer,
+            _ => ActivityKind.Internal // Default for unknown values
+        };
     }
 }
