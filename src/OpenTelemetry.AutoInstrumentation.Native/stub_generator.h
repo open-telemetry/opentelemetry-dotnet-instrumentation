03468d0d69afa792f13692e6117fc3c4421c2e43
Rhynier Myburgh <rmyburgh@cisco.com>
2025-11-10 17:27:15 -0800

    Moving refactor to a different PR

--- src/OpenTelemetry.AutoInstrumentation.Native/stub_generator.h
+++ src/OpenTelemetry.AutoInstrumentation.Native/stub_generator.h
@@ -31,22 +31,9 @@ public:
 
     ~StubGenerator();
 
-#ifdef _WIN32
-    void PatchAppDomainCreate(const ModuleID module_id);
-    HRESULT GenerateLoaderMethod(const ModuleID module_id, mdMethodDef* ret_method_token);
-#endif
-
     HRESULT PatchProcessStartupHooks(const ModuleID module_id, const WSTRING& startup_hook_assembly_path);
 
 private:
-#ifdef _WIN32
-    HRESULT GenerateLoaderType(const ModuleID module_id,
-                               mdTypeDef*     loader_type,
-                               mdMethodDef*   init_method,
-                               mdMethodDef*   patch_app_domain_setup_method);
-    HRESULT ModifyAppDomainCreate(const ModuleID module_id, mdMethodDef patch_app_domain_setup_method);
-#endif
-
     HRESULT ModifyProcessStartupHooks(const ModuleID module_id, mdMethodDef patch_startup_hook_method);
     HRESULT GenerateHookFixup(const ModuleID module_id,
                               mdTypeDef*     hook_fixup_type,
