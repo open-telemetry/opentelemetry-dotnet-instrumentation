1d037c9e7c900315882fe778bf7c77a57b11b37f
Yevhenii Solomchenko <ysolomchenko@splunk.com>
2026-01-08 09:39:18 +0100

    add header guard

--- src/OpenTelemetry.AutoInstrumentation.Native/configuration.h
+++ src/OpenTelemetry.AutoInstrumentation.Native/configuration.h
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifndef OTEL_CLR_CONFIGURATION_H_
+#define OTEL_CLR_CONFIGURATION_H_
+
 namespace trace
 {
 
@@ -10,3 +13,5 @@ bool IsSqlClientNetFxILRewriteEnabled();
 void SetSqlClientNetFxILRewriteEnabled(bool enabled);
 
 } // namespace trace
+
+#endif // OTEL_CLR_CONFIGURATION_H_
