f2e960ebc2462762f358ccb3cd9b193b4d52423d
Piotr Kiełkowicz <pkielkow@cisco.com>
2026-01-19 14:38:27 +0100

    Common place to extract versions in tests

---
+++ test/IntegrationTests/Helpers/VersionHelper.cs
@@ -0,0 +1,27 @@
+﻿// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Reflection;
+
+namespace IntegrationTests.Helpers;
+
+internal static class VersionHelper
+{
+    public static string AutoInstrumentationVersion
+    {
+        get
+        {
+            return typeof(OpenTelemetry.AutoInstrumentation.Constants).Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion.Split('+')[0];
+        }
+    }
+
+    public static string SdkVersion
+    {
+        get
+        {
+            return typeof(OpenTelemetry.Resources.Resource).Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion.Split('+')[0];
+        }
+    }
+}
