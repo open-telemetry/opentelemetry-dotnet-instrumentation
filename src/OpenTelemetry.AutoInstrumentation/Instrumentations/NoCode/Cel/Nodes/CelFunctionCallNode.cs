d21ec1e951bd2fcb01a337a240059b730edb38eb
Piotr Kiełkowicz <pkielkow@cisco.com>
2026-03-06 10:00:32 +0100

    redundant using

--- src/OpenTelemetry.AutoInstrumentation/Instrumentations/NoCode/Cel/Nodes/CelFunctionCallNode.cs
+++ src/OpenTelemetry.AutoInstrumentation/Instrumentations/NoCode/Cel/Nodes/CelFunctionCallNode.cs
@@ -1,8 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-using System.Text;
-
 namespace OpenTelemetry.AutoInstrumentation.Instrumentations.NoCode.Cel;
 
 /// <summary>
