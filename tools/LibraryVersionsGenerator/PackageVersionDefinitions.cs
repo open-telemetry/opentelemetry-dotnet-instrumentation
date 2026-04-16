4636affdb82b1e4b35ee11621ef0c2cd98006618
Piotr Kiełkowicz <pkielkow@cisco.com>
2026-04-16 11:41:05 +0200

    Bump test/infra vulnerable packages (#4999)

--- tools/LibraryVersionsGenerator/PackageVersionDefinitions.cs
+++ tools/LibraryVersionsGenerator/PackageVersionDefinitions.cs
@@ -217,8 +217,11 @@ internal static class PackageVersionDefinitions
             Versions =
             [
                 // new("8.0.0"), - transitive vulnerabilities https://github.com/advisories/GHSA-8g4q-xg66-9fp4, <=8.2.3
-                new("8.2.5"),
-                new("9.1.0", supportedTargetFrameworks: ["net10.0", "net9.0", "net8.0"], supportedExecutionFrameworks: ["net10.0", "net9.0", "net8.0"]), // breaking change, new Meter name
+                new("8.2.6"),
+                // new("9.1.0", supportedTargetFrameworks: ["net10.0", "net9.0", "net8.0"], supportedExecutionFrameworks: ["net10.0", "net9.0", "net8.0"]), // breaking change, new Meter name - transitive vulnerabilities <= 9.2.9
+                new("9.2.10", supportedTargetFrameworks: ["net10.0", "net9.0", "net8.0"], supportedExecutionFrameworks: ["net10.0", "net9.0", "net8.0"]),
+                // new("10.0.0", supportedTargetFrameworks: ["net10.0"], supportedExecutionFrameworks: ["net10.0"]), // transitive vulnerabilities <= 10.1.2
+                new("10.1.3", supportedTargetFrameworks: ["net10.0"], supportedExecutionFrameworks: ["net10.0"]),
                 new("*", supportedTargetFrameworks: ["net10.0"], supportedExecutionFrameworks: ["net10.0"])
             ]
         },
