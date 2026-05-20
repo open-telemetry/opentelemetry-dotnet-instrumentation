787a651e08970edfb5b7eb2bbdf58b1e440bb471
Piotr Kiełkowicz <pkielkow@cisco.com>
2026-05-20 09:23:41 +0200

    InitialVersion fo Assembly Redirections

--- test/IntegrationTests/LibraryVersions.g.cs
+++ test/IntegrationTests/LibraryVersions.g.cs
@@ -485,6 +485,9 @@ public static partial class LibraryVersion
 #if NET462 || NET11_0 || NET10_0
                 "10.0.10",
 #endif
+#if NET462 || NET11_0 || NET10_0
+                "11.0.0-preview.4.26230.115",
+#endif
 #endif
             ];
             return theoryData;
