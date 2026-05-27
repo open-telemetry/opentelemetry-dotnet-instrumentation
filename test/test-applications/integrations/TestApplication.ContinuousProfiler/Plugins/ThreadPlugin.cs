d469a7466b88f872812d39d6817db68dfd43a9ad
RassK <rkuusmann@splunk.com>
2026-05-27 18:51:16 +0300

    fix continuous profiler tests

--- test/test-applications/integrations/TestApplication.ContinuousProfiler/Plugins/ThreadPlugin.cs
+++ test/test-applications/integrations/TestApplication.ContinuousProfiler/Plugins/ThreadPlugin.cs
@@ -1,31 +1,18 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+using OpenTelemetry.AutoInstrumentation.PluginApi.ContinuousProfiling;
 using OpenTelemetry.Resources;
+using TestApplication.ContinuousProfiler.Plugins;
 
 namespace TestApplication.ContinuousProfiler;
 
 #pragma warning disable CA1515 // Consider making public types internal. Needed for AutoInstrumentation plugin loading.
-public class ThreadPlugin
+public class ThreadPlugin : BasePlugin, IContinuousProfilerPlugin
 #pragma warning restore CA1515 // Consider making public types internal. Needed for AutoInstrumentation plugin loading.
 {
 #pragma warning disable CA1822 // Mark members as static. Needed for AutoInstrumentation plugin loading.
-    public Tuple<bool, uint, bool, uint, TimeSpan, TimeSpan, object> GetContinuousProfilerConfiguration()
-    {
-        var threadSamplingEnabled = true;
-        var threadSamplingInterval = 1000u;
-        var allocationSamplingEnabled = false;
-        var maxMemorySamplesPerMinute = 200u;
-        var exportInterval = TimeSpan.FromMilliseconds(500);
-        var exportTimeout = TimeSpan.FromMilliseconds(5000);
-#pragma warning disable CA2000 // Dispose objects before losing scope. It should be handled by the AutoInstrumentation.
-        object continuousProfilerExporter = new OtlpOverHttpExporter(TimeSpan.FromMilliseconds(threadSamplingInterval), new SampleNativeFormatParser());
-#pragma warning restore CA2000 // Dispose objects before losing scope. It should be handled by the AutoInstrumentation.
-
-        return Tuple.Create(threadSamplingEnabled, threadSamplingInterval, allocationSamplingEnabled, maxMemorySamplesPerMinute, exportInterval, exportTimeout, continuousProfilerExporter);
-    }
-
-    public ResourceBuilder ConfigureResource(ResourceBuilder builder)
+    public override ResourceBuilder ConfigureResource(ResourceBuilder builder)
 #pragma warning restore CA1822 // Mark members as static. Needed for AutoInstrumentation plugin loading.
     {
 #if NET
@@ -40,5 +27,21 @@ public class ThreadPlugin
         // TODO hack to fetch resources from the SDK and store them to be able to sent to OTLP Profiling
         ResourcesProvider.Configure(builder);
         return builder;
+    }
+
+    public ContinuousProfilerConfiguration GetFirstContinuousProfilerConfiguration()
+    {
+        var threadSamplingInterval = 1000u;
+
+        return new ContinuousProfilerConfiguration
+        {
+            ThreadSamplingEnabled = true,
+            ThreadSamplingInterval = threadSamplingInterval,
+            AllocationSamplingEnabled = false,
+            MaxMemorySamplesPerMinute = 200u,
+            ExportInterval = TimeSpan.FromMilliseconds(500),
+            ExportTimeout = TimeSpan.FromMilliseconds(5000),
+            Exporter = new OtlpOverHttpExporter(TimeSpan.FromMilliseconds(threadSamplingInterval), new SampleNativeFormatParser())
+        };
     }
 }
