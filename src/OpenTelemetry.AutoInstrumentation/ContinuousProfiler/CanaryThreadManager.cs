1af015450900f9f0617db58fa7d4e39723bb6526
Piotr Kiełkowicz <pkielkow@cisco.com>
2026-05-29 13:25:32 +0200

    Fix dotnet-format

--- src/OpenTelemetry.AutoInstrumentation/ContinuousProfiler/CanaryThreadManager.cs
+++ src/OpenTelemetry.AutoInstrumentation/ContinuousProfiler/CanaryThreadManager.cs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #if NETFRAMEWORK
-using System.Diagnostics;
 using OpenTelemetry.AutoInstrumentation.Logging;
 
 namespace OpenTelemetry.AutoInstrumentation.ContinuousProfiler;
