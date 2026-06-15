a746a16505f68cd52d586c4bc4c58bf25e88945a
Piotr Kiełkowicz <pkielkow@cisco.com>
2026-06-15 13:18:38 +0200

    redirect checks

--- test/IntegrationTests/LibraryVersions.g.cs
+++ test/IntegrationTests/LibraryVersions.g.cs
@@ -476,7 +476,7 @@ public static partial class LibraryVersion
                 "10.0.0",
 #endif
 #if NET462 || NET10_0
-                "10.0.8",
+                "10.0.9",
 #endif
 #endif
             ];
