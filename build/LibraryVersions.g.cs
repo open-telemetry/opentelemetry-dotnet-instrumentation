37fece23a1ab4c38d953fbb09a2611effe275067
Piotr Kiełkowicz <pkielkow@cisco.com>
2026-08-03 20:15:35 +0200

    revert redis

--- build/LibraryVersions.g.cs
+++ build/LibraryVersions.g.cs
@@ -187,7 +187,7 @@ public static partial class LibraryVersion
             [
                 new("2.6.122"),
                 new("2.12.14"),
-                new("3.1.3", supportedFrameworks: [ "net10.0", "net9.0", "net8.0" ]),
+                new("3.0.47-preview", supportedFrameworks: [ "net10.0", "net9.0", "net8.0" ]),
             ]
         },
         {
