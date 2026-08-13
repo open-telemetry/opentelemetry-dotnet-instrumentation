a8b41164f55a8b5725d4d27acdbcf87a41169762
Piotr Kiełkowicz <pkielkow@cisco.com>
2026-08-13 07:03:21 +0200

    generated files

--- test/IntegrationTests/LibraryVersions.g.cs
+++ test/IntegrationTests/LibraryVersions.g.cs
@@ -244,7 +244,7 @@ public static partial class LibraryVersion
                 string.Empty,
 #else
                 "2.0.0",
-                "2.6.1",
+                "2.6.2",
 #endif
             ];
             return theoryData;
