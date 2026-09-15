352afb381e539197fe2432c3b82f521b76e8da74
Piotr Kiełkowicz <pkielkow@cisco.com>
2026-09-15 07:50:41 +0200

    generated files

--- test/IntegrationTests/LibraryVersions.g.cs
+++ test/IntegrationTests/LibraryVersions.g.cs
@@ -227,7 +227,7 @@ public static partial class LibraryVersion
                 "3.7.0",
 #endif
 #if NET10_0 || NET9_0 || NET8_0 || NET462
-                "3.11.1",
+                "3.11.2",
 #endif
 #endif
             ];
@@ -304,7 +304,7 @@ public static partial class LibraryVersion
                 "10.1.3",
 #endif
 #if NET10_0
-                "10.2.8",
+                "10.2.9",
 #endif
 #endif
             ];
@@ -423,7 +423,7 @@ public static partial class LibraryVersion
                 "3.1.0",
 #endif
 #if NET10_0 || NET9_0 || NET8_0
-                "3.1.31",
+                "3.2.1",
 #endif
 #endif
             ];
@@ -461,7 +461,7 @@ public static partial class LibraryVersion
 #else
                 "1.8.2",
                 "1.9.2",
-                "2.15.0",
+                "2.15.1",
 #endif
             ];
             return theoryData;
