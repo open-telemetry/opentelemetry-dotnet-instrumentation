94610444ccac50beb00dfc2bee812289b364334a
Piotr Kiełkowicz <pkielkow@cisco.com>
2026-09-24 07:28:47 +0200

    add missing version to the matrix

--- build/LibraryVersions.g.cs
+++ build/LibraryVersions.g.cs
@@ -191,6 +191,7 @@ public static partial class LibraryVersion
                 new("2.12.14"),
                 new("3.0.25", supportedFrameworks: [ "net10.0", "net9.0", "net8.0" ]),
                 new("3.1.0", supportedFrameworks: [ "net10.0", "net9.0", "net8.0" ]),
+                new("3.3.0", supportedFrameworks: [ "net10.0", "net9.0", "net8.0" ]),
                 new("3.3.1", supportedFrameworks: [ "net10.0", "net9.0", "net8.0" ]),
             ]
         },
--- test/IntegrationTests/LibraryVersions.g.cs
+++ test/IntegrationTests/LibraryVersions.g.cs
@@ -423,6 +423,9 @@ public static partial class LibraryVersion
                 "3.1.0",
 #endif
 #if NET10_0 || NET9_0 || NET8_0
+                "3.3.0",
+#endif
+#if NET10_0 || NET9_0 || NET8_0
                 "3.3.1",
 #endif
 #endif
